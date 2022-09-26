091b331aa5e6a81f6088be9f1676f6550a8be7d3
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-09-26 21:35:16 +0900

    Update 02_Fibonacci.cpp

--- cpp/02_Fibonacci.cpp
+++ cpp/02_Fibonacci.cpp
@@ -2,15 +2,16 @@
 	피보나치 수열: 성능 평가
 
 	1) 재귀적 용법
-	2) 비재귀적 용법: 동적 프로그래밍
+	2) 비재귀적 용법
 */
 
 #include <iostream>
-#include <ctime>		// clock, clock_t, CLOCKS_PER_SEC
+#include <ctime>	// clock, clock_t, CLOCKS_PER_SEC
 using namespace std;
 // clock 함수: 프로그램에 의해 프로세서가 소비된 시간을 반환하는 함수
 // CLOCKS_PER_SEC: 초당 clock ticks의 수를 나타낸 매크로 상수(시스템에 따라 기본값이 다름)
 
+// 피보나치 수열: 재귀적 용법
 int  Fibo(int num);
 
 int main(void)
@@ -24,13 +25,13 @@ int main(void)
 	clock_t		start, finish;
 	double		seconds;
 
-	start = clock();		// 시작 시간
+	start = clock();	// 시작 시간
 	for (int i = 1; i <= num; i++) {
 		cout.width(8);
 		if (i % 5)	cout << Fibo(i);
 		else	cout << Fibo(i) << endl;
 	}
-	finish = clock();		// 종료 시간
+	finish = clock();	// 종료 시간
 
 	seconds = (double)(finish - start) / CLOCKS_PER_SEC;  	// CPU 시간
 	cout << num << "번째 피보나치 수열 계산 시간: " << seconds << endl;
