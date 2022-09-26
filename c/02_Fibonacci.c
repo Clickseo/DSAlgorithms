8359767ba5f24f88a1a857c92b66d83c7effe33e
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-09-26 21:32:34 +0900

    Update 02_Fibonacci.c

--- c/02_Fibonacci.c
+++ c/02_Fibonacci.c
@@ -10,6 +10,7 @@
 // clock 함수: 프로그램에 의해 프로세서가 소비된 시간을 반환하는 함수
 // CLOCKS_PER_SEC: 초당 clock ticks의 수를 나타낸 매크로 상수(시스템에 따라 기본값이 다름)
 
+// 피보나치 수열: 재귀적 용법
 int  Fibo(int num);
 
 int main(void)
@@ -24,13 +25,13 @@ int main(void)
 	clock_t		start, finish;
 	double		seconds;
 
-	start = clock();		// 시작 시간
+	start = clock();	// 시작 시간
 	for (int i = 1; i <= num; i++) {
 		if (i % 5)	printf("%8d", Fibo(i));
 		else		printf("%8d\n", Fibo(i));
 	}
 	printf("\n");
-	finish = clock();		// 종료 시간
+	finish = clock();	// 종료 시간
 
 	seconds = (double)(finish - start) / CLOCKS_PER_SEC;  	// CPU 시간
 	printf("%d 번째 피보나치 수열 계산 시간: %lf 초 \n", num, seconds);
