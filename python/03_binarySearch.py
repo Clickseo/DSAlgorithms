c2bb1c54b43a852a51a6a53f126ffb2facaa4e64
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-09-26 21:27:50 +0900

    Update 03_binarySearch.py

--- python/03_binarySearch.py
+++ python/03_binarySearch.py
@@ -26,6 +26,7 @@ if __name__ == '__main__' :
     while True :
         key = int(input("검색 데이터 입력(검색 종료: 0): "))
         if key == 0 : break
+		
         index = binarySearch(sList, 0, len(sList)-1, key)
         if index == None : print("없다고!!!")
         else : print(f"검색된 데이터: {index+1}번째 {sList[index]}")
