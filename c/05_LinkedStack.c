567a426972acbc6445fefbebac0610143bb2dc7f
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 04:48:39 +0900

    Update 05_LinkedStack.c

--- c/05_LinkedStack.c
+++ c/05_LinkedStack.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>		      // malloc, free
-#include "LinkedStack.h"		// ListStack, stackNode
+#include <stdlib.h>		// malloc, free
+#include "LinkedStack.h"	// ListStack, stackNode
 
 // 빈 스택 생성
 LinkedStack* stackCreate(void) {
