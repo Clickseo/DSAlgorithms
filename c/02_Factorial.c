55f4cd3669bb33900727da3d4f0c722125ce053d
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 00:29:16 +0900

    Create 02_Factorial.c

---
+++ c/02_Factorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int  Factorial(int num); 
+int main(void)	
+{
+	int	num;
+
+	printf("임의의 정수: ");
+	scanf_s("%d", &num);
+  // scanf("%d", &num);
+	
+  printf("%d Factorial: %d \n", num, Factorial(num) );
+	return 0;
+}
+
+# Factorial: 재귀적 용법
+int  Factorial(int num)	{
+	if(num == 0)
+		return 1;	
+	return num * Factorial (num - 1);
+}
+
+/*
+# Factorial: 반복적 용법
+int  Factorial(int num)	{
+	int	res = 1;
+	for (int i = 1; i <= num; i++)
+		res  =  res  *  i;
+	return  res;
+}
+*/
