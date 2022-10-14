88f210ec679ee5d6379fad8f8d307ccb026641d1
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 04:42:48 +0900

    Create LinkedStack.h

---
+++ c/LinkedStack.h
@@ -0,0 +1,31 @@
+/*	
+	스택: 알고리즘 구현 -- 연결자료구조(단순연결리스트)
+  
+    - stackNode
+    - LinkedStack
+*/
+
+// #pragma once
+#ifndef __ListStack_H__
+#define __ListStack_H__
+
+typedef int element;
+typedef struct _stackNode {
+	element		          data;
+	struct _stackNode*	link;
+}stackNode;
+
+typedef struct _LinkedStack {
+	stackNode* top;
+}LinkedStack;
+
+#endif
+
+LinkedStack*	stackCreate(void);
+void			    stackDestroy(LinkedStack* s);
+stackNode*	  makeStackNode(int data);
+void			    push(LinkedStack* s, element data);
+element		    pop(LinkedStack* s);
+element		    peek(LinkedStack* s);
+_Bool			    isEmpty(LinkedStack* s);
+void			    printStack(LinkedStack* s);
