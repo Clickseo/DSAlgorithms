eefc7770abf0451e72a758f90d9224785a788ff0
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 01:12:18 +0900

    Update 02_Factorial.c

--- c/02_Factorial.c
+++ c/02_Factorial.c
@@ -1,6 +1,6 @@
 /*
-  계승(Factorial) 구하기
-    1) 재귀적 용법
+	계승(Factorial) 구하기
+		1) 재귀적 용법
 		2) 비재귀적 용법
 */
 
@@ -12,9 +12,9 @@ int main(void)
 
 	printf("임의의 정수: ");
 	scanf_s("%d", &num);
-  // scanf("%d", &num);
+	// scanf("%d", &num);
 	
-  printf("%d Factorial: %d \n", num, Factorial(num) );
+	printf("%d Factorial: %d \n", num, Factorial(num) );
 	return 0;
 }
 
