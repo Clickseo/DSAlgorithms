23bccaf3db7a70e4a249340a9f5b9927727deb9c
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 04:43:46 +0900

    Create 05_LinkedStack.h

---
+++ c/05_LinkedStack.h
@@ -0,0 +1,24 @@
+// #pragma once
+#ifndef __ListStack_H__
+#define __ListStack_H__
+
+typedef int element;
+typedef struct _stackNode {
+	element		          data;
+	struct _stackNode*	link;
+}stackNode;
+
+typedef struct _LinkedStack {
+	stackNode* top;
+}LinkedStack;
+
+#endif
+
+LinkedStack*	stackCreate(void);
+void			    stackDestroy(LinkedStack* s);
+stackNode*		makeStackNode(int data);
+void			    push(LinkedStack* s, element data);
+element		    pop(LinkedStack* s);
+element		    peek(LinkedStack* s);
+_Bool			    isEmpty(LinkedStack* s);
+void			    printStack(LinkedStack* s);
