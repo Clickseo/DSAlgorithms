b4c9c580e6ccd3441beebf441cc5d7034bfcca64
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 01:06:46 +0900

    Update 02_Factorial.c

--- c/02_Factorial.c
+++ c/02_Factorial.c
@@ -1,6 +1,12 @@
+'''
+  계승(Factorial) 구하기
+	  1) 재귀적 용법
+	  2) 비재귀적 용법
+'''
+
 #include <stdio.h>
-int  Factorial(int num); 
-int main(void)	
+int  Factorial(int num);
+int main(void)
 {
 	int	num;
 
@@ -12,15 +18,15 @@ int main(void)
 	return 0;
 }
 
-# Factorial: 재귀적 용법
+# 계승(Factorial) 구하기: 재귀적 용법
 int  Factorial(int num)	{
 	if(num == 0)
-		return 1;	
+		return 1;
 	return num * Factorial (num - 1);
 }
 
 /*
-# Factorial: 반복적 용법
+# 계승(Factorial) 구하기: 반복적 용법
 int  Factorial(int num)	{
 	int	res = 1;
 	for (int i = 1; i <= num; i++)
