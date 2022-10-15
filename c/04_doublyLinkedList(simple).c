ad515a257a433480267acf8aceb91fe3d482ee3e
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-10-15 10:47:57 +0900

    Update 04_doublyLinkedList(simple).c

--- c/04_doublyLinkedList(simple).c
+++ c/04_doublyLinkedList(simple).c
@@ -1,16 +1,16 @@
 /*
 	이중 연결 리스트: 알고리즘 구현
-		- DNode			  : 노드(data, Llink, Rlink)
+		- DNode		: 노드(data, Llink, Rlink)
 		- SLinkedList	: head
 */
 
 #include <stdio.h>
-#include <stdlib.h>		// exit
+#include <stdlib.h>	// exit
 #include <stdbool.h>	// bool, true, false
 
 // 노드: 데이터, 링크
 typedef struct _DNode {
-	int				      data;
+	int		data;
 	struct  _DNode* Llink;
 	struct  _DNode* Rlink;
 }DNode;
@@ -23,15 +23,15 @@ typedef struct _DLinkedList {
 }DLinkedList;
 
 // 단순 연결 리스트: 함수 정의
-_Bool	  isEmpty(DLinkedList* dList);				        // 빈 리스트 여부 판단
-int		  countDNode(DLinkedList* dList);				      // 탐색: 노드의 총 개수(count)
-DNode*	frontDNode(DLinkedList* dList);				      // 탐색: 첫 번째 노드
-DNode*	rearDNode(DLinkedList* dList);				      // 탐색: 맨 마지막 노드(tail)
-DNode*	makeDNode(int num);							            // 새로운 노드 생성
+_Bool	isEmpty(DLinkedList* dList);			// 빈 리스트 여부 판단
+int	countDNode(DLinkedList* dList);			// 탐색: 노드의 총 개수(count)
+DNode*	frontDNode(DLinkedList* dList);			// 탐색: 첫 번째 노드
+DNode*	rearDNode(DLinkedList* dList);			// 탐색: 맨 마지막 노드(tail)
+DNode*	makeDNode(int num);				// 새로운 노드 생성
 void    addRear(DLinkedList* dList, DNode* nNode);	// 노드 삽입: 맨 마지막 노드로...
-void	  removeFront(DLinkedList* dList);			      // 노드 삭제: 첫 번째 노드를...
-void	  printDLinkedList(DLinkedList* dList);		    // 리스트의 전체 노드 출력
-void	  revPrintDLinkedList(DLinkedList* dList);	  // 리스트의 전체 노드 출력
+void	removeFront(DLinkedList* dList);		// 노드 삭제: 첫 번째 노드를...
+void	printDLinkedList(DLinkedList* dList);		// 리스트의 전체 노드 출력
+void	revPrintDLinkedList(DLinkedList* dList);	// 리스트의 전체 노드 출력
 
 int main(void)
 {
@@ -52,7 +52,7 @@ int main(void)
 			break;
 
 		DNode* newDNode = makeDNode(num);	// 새로운 노드 생성		
-		addRear(dList, newDNode);			    // 맨 마지막 노드로 삽입
+		addRear(dList, newDNode);		// 맨 마지막 노드로 삽입
 	}
 
 	// 전체 원소 출력
@@ -61,7 +61,7 @@ int main(void)
 		return 0;
 	}
 	printf("\n ### 입력된 데이터 ### \n\n");
-	printDLinkedList(dList);		  // 순방향 출력
+	printDLinkedList(dList);		// 순방향 출력
 	revPrintDLinkedList(dList);		// 역방향 출력
 
 	// 전체 원소 삭제
@@ -82,7 +82,7 @@ int		countDNode(DLinkedList* dList) {
 	if (isEmpty(dList))
 		return 0;
 
-	int		count = 0;
+	int	count = 0;
 	DNode* rNode = dList->head;
 	while (rNode->Rlink) {
 		count++;
