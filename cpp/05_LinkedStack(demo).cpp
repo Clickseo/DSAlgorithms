d7f19a85087c66fd36cf658f29163956e2622313
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-05 05:34:52 +0900

    Create 05_LinkedStack(demo).cpp

---
+++ cpp/05_LinkedStack(demo).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "LinkedStack.h"
+using namespace std;
+
+int main(void)
+{
+	LinkedStack<int>	s;
+	int               num, choice;
+
+	while (true) {
+		system("cls");
+		cout << "\n ### 스택 구현: 단순 연결 리스트 ### \n" << endl;
+		cout << "1) 데이터 삽입(PUSH)" << endl;
+		cout << "2) 데이터 삭제(POP)" << endl;
+		cout << "3) 전체 출력" << endl;
+		cout << "4) 프로그램 종료 \n" << endl;
+		cout << "메뉴 선택 : ";
+		cin >> choice;
+
+		switch (choice) {
+        case 1: cout << "\n삽입 할 데이터 입력: ";
+                cin >> num;		  s.push(num);	  break;
+        case 2: cout << "삭제 된 데이터: " << s.top() << endl;
+                s.pop();		    break;
+        case 3: s.printStack(); break;
+        case 4: cout << "프로그램 종료..." << endl;
+                return 0;
+        default: cout << "잘못 선택 하셨습니다." << endl;
+		}
+		system("pause");
+	}	
+	return 0;	// s.~ListStack();
+}
