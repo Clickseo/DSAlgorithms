dcaf02cdbfc16c50d06ca5e15e5cdcd55ca2fd0a
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-05 05:38:12 +0900

    Update 05_LinkedStack(demo).cpp

--- cpp/05_LinkedStack(demo).cpp
+++ cpp/05_LinkedStack(demo).cpp
@@ -1,3 +1,8 @@
+/*
+	스택: 알고리즘 구현 -- 단순연결리스트
+		- main	: 데이터 삽입(push).삭제(pop), 전체 원소 출력
+*/
+
 #include <iostream>
 #include "LinkedStack.h"
 using namespace std;
@@ -5,7 +10,7 @@ using namespace std;
 int main(void)
 {
 	LinkedStack<int>	s;
-	int               num, choice;
+	int			num, choice;
 
 	while (true) {
 		system("cls");
@@ -18,14 +23,14 @@ int main(void)
 		cin >> choice;
 
 		switch (choice) {
-        case 1: cout << "\n삽입 할 데이터 입력: ";
-                cin >> num;		  s.push(num);	  break;
-        case 2: cout << "삭제 된 데이터: " << s.top() << endl;
-                s.pop();		    break;
-        case 3: s.printStack(); break;
-        case 4: cout << "프로그램 종료..." << endl;
-                return 0;
-        default: cout << "잘못 선택 하셨습니다." << endl;
+			case 1: cout << "\n삽입 할 데이터 입력: ";
+				cin >> num;	  s.push(num);	  break;
+			case 2: cout << "삭제 된 데이터: " << s.top() << endl;
+				s.pop();	break;
+			case 3: s.printStack(); break;
+			case 4: cout << "프로그램 종료..." << endl;
+				return 0;
+			default: cout << "잘못 선택 하셨습니다." << endl;
 		}
 		system("pause");
 	}	
