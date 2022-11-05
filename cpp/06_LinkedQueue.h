d24df0f135b1ef3d250bbc203023e9258ffe1ceb
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-06 00:35:45 +0900

    Update 06_LinkedQueue.h

--- cpp/06_LinkedQueue.h
+++ cpp/06_LinkedQueue.h
@@ -3,11 +3,11 @@
 		파일명: LinkedQueue.h
 		클래스: SNode
 		클래스: LinkedQueue
-			- 생성자.소멸자			: LinkedQueue, ~LinkedQueue
-			- 데이터 삽입.삭제		: push, pop		// enQueue, deQueue
-			- 데이터 확인			: front, back
-			- 큐 크기				: size
-			- 빈 스택 여부 판단		: empty
+			- 생성자.소멸자	  : LinkedQueue, ~LinkedQueue
+			- 데이터 삽입.삭제	 : push, pop			// enQueue, deQueue
+			- 데이터 확인	   : front, back
+			- 큐 크기		    : size
+			- 빈 스택 여부 판단	 : empty
 			- 스택의 전체 원소 출력	: printStack
 */
 
@@ -74,7 +74,7 @@ void	LinkedQueue<E>::push(const E& e) {		// enQueue
 }
 
 template <typename E>
-void	LinkedQueue<E>::pop(void) {				// deQueue
+void	LinkedQueue<E>::pop(void) {			// deQueue
 	if (empty()) error("STACK IS EMPTY");		// throw "ERROR::STACK IS EMPTY";
 	SNode<int>* temp = __front;
 	E data = temp->__data;
