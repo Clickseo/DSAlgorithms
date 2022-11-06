28efce23ebd80c19d932185b96ba150a5cb1ed09
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-07 00:13:03 +0900

    Update 04_LinkedSNode.c

--- c/04_LinkedSNode.c
+++ c/04_LinkedSNode.c
@@ -1,11 +1,11 @@
 /*
 	단순 연결 리스트 : 알고리즘 구현(C)
 		파일명: LinkedSNode.c
-			- 노드		: SNode(data, link)
+			- 노드 : SNode(data, link)
 */
 
 #include <stdio.h>
-#include <stdlib.h>			  // malloc, free
+#include <stdlib.h>		// malloc, free
 #include "LinkedSNode.h"	// SNode
 
 // 새로운 노드(data, link) 생성
