18d4757fd56c3bd00c8bdc9ed86d2f13bb4efba1
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-07 00:16:10 +0900

    Update 05_LlinkedStack.h

--- c/05_LlinkedStack.h
+++ c/05_LlinkedStack.h
@@ -1,26 +1,22 @@
 /*
-	스택: 알고리즘 구현(C) --단순연결리스트
+	스택: 알고리즘 구현(C) -- 단순 연결 리스트
 		파일명: LinkedStack.h
-			- 스택 생성.소멸       : stackCreate, stackDestroy
-			- 데이터 삽입.삭제     : push, pop
-			- 데이터 확인(peek)    : top
-			- 빈 스택 여부 판단    : stackEempty
-			- 스택 크기            : stackSize
-			- 스택의 전체 원소 출력 : printStack
+			- 스택 생성.소멸        : stackCreate, stackDestroy
+			- 데이터 삽입.삭제      : push, pop
+			- 데이터 확인(peek)     : top
+			- 빈 스택 여부 판단      : stackEempty
+			- 스택 크기             : stackSize
+			- 스택의 전체 원소 출력	: printStack
 */
 
 // #pragma once
+
+#include "LinkedSNode.h"	// SNode
+typedef		int		element;
+
+// 스택 생성: LinkedStack
 #ifndef __LinkedStack_H__
 #define __LinkedStack_H__
-typedef	int	element;
-
-// 노드: SNode(data, link)
-typedef struct __SNode {
-	element		__data;
-	struct __SNode*	__link;
-}SNode;
-
-// 스택 생성: LinkedStack, stackNode
 typedef struct __LinkedStack {
 	SNode*	__top;
 	int	__count;
@@ -28,7 +24,6 @@ typedef struct __LinkedStack {
 #endif
 
 // 스택 구현(C): 스택 생성 및 활용
-SNode*		makeSNode(element data);
 LinkedStack*	stackCreate(void);
 void		stackDestroy(LinkedStack* s);
 void		push(LinkedStack* s, element data);
