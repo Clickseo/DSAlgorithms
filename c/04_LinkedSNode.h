300ee976af9cc76d936cc2507d5998cb7981019a
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-07 00:14:14 +0900

    Update 04_LinkedSNode.h

--- c/04_LinkedSNode.h
+++ c/04_LinkedSNode.h
@@ -1,18 +1,18 @@
 /*
 	단순 연결 리스트 : 알고리즘 구현(C)
 		파일명: LinkedSNode.h
-			- 노드		: SNode(data, link)
+			- 노드 : SNode(data, link)
 */
 
 #pragma once
 
-typedef	int		element;
+typedef	int	element;
 
 // 노드: SNode(data, link)
 #ifndef __SNode_H__
 #define __SNode_H__
 typedef struct __SNode {
-	element			__data;
+	element		__data;
 	struct __SNode* __link;
 }SNode;
 #endif
