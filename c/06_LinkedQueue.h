61e28a9384ce0a9b8f709fd00734b95920b7db11
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 02:42:53 +0900

    Update 06_LinkedQueue.h

--- c/06_LinkedQueue.h
+++ c/06_LinkedQueue.h
@@ -1,17 +1,16 @@
 /*
 	큐: 알고리즘 구현(C) -- 단순 연결 리스트
 		파일명: LinkedQueue.h
-			- 큐의 생성.소멸       : queueCreate, queueDestroy
-			- 데이터 삽입.삭제     : enQueue, deQueue
-			- 데이터 확인(peek)    : front, back
-			- 빈 스택 여부 판단    : queueEempty
-			- 큐의 크기            : queueSize
-			- 큐의 전체 원소 출력  : printStack
+			- 큐의 생성.소멸		: queueCreate, queueDestroy
+			- 데이터 삽입.삭제		: enQueue, deQueue
+			- 데이터 확인(peek)		: front, back
+			- 빈 스택 여부 판단		: queueEempty
+			- 큐의 크기				: queueSize
+			- 큐의 전체 원소 출력	: printQueue
 */
 
 // #pragma once
-#include "LinkedNode.h"		// SNode
-typedef	int	element;
+#include "LinkedNode.h"		// SNode, DNode
 
 // 큐 생성: LinkedQueue
 #ifndef __LinkdedQueue_H__
