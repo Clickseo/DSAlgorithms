bdb944315733122a188056dfd2e079a9cc58be37
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 03:08:33 +0900

    Update 07_LinkedBinaryTree(demo).c

--- c/07_LinkedBinaryTree(demo).c
+++ c/07_LinkedBinaryTree(demo).c
@@ -5,8 +5,8 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>			    // exit
-#include "LinkedBinaryTree.h"		// DNode
+#include <stdlib.h>		// exit
+#include "LinkedBinaryTree.h"	// DNode
 #define bufferMAXSIZE 1024
 
 int main(void)
