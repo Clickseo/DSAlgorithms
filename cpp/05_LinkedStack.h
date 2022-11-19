bb4f9050569bcafb36ec4f097a2a00b214218aac
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 01:18:02 +0900

    Update 05_LinkedStack.h

--- cpp/05_LinkedStack.h
+++ cpp/05_LinkedStack.h
@@ -3,12 +3,12 @@
 		파일명: LinkedStack.h
 		클래스: SNode
 		클래스: LinkedStack
-			- 생성자.소멸자        : LinkedStack, ~LinkedStack
-			- 데이터 삽입.삭제     : push, pop, top
-			- 데이터 확인(peek)    : top
-			- 빈 스택 여부 판단    : empty
-			- 스택 크기            : size
-			- 스택의 전체 원소 출력 : printStack
+			- 생성자.소멸자			: LinkedStack, ~LinkedStack
+			- 데이터 삽입.삭제		: push, pop
+			- 데이터 확인(peek)		: top
+			- 빈 스택 여부 판단		: empty
+			- 스택 크기				: size
+			- 스택의 전체 원소 출력	: printStack
 */
 
 #include <iostream>
@@ -23,39 +23,44 @@ inline void error(const char* message) {
 template <typename E>
 class SNode {
 private:
-	E		__data;
+	E			__data;
 	SNode<E>*	__link;
 	template <typename E> friend class LinkedStack;
 public:
-	SNode();
+	SNode(const E& data);
+	E	getData(void) const;
 };
 
-// 새로운 노드(data, link) 생성
+// SNode: 생성자와 메소드 정의
 template <typename E>
-SNode<E>::SNode() : __data(__data), __link(NULL) {}
+SNode<E>::SNode(const E& data) :
+	__data(data), __link(nullptr) {}
+
+template <typename E>
+E   SNode<E>::getData(void) const { return __data; }
 
 // LinkedStack class
 template <typename E>
 class LinkedStack {
 private:
 	SNode<E>*	__top;
-	int		__count;
+	int			__count;
 public:
 	LinkedStack();
 	~LinkedStack();
 	void	push(const E& e);
 	void	pop(void);
-	E&	top(void) const;
+	E&		top(void) const;
 	bool	empty(void) const;
-	int	size(void) const;
+	int		size(void) const;
 	void	printStack(void) const;
 };
 
-// 생성자: 빈 스택 생성
+// LinkedStack: 생성자(소멸자)와 메소드 정의
 template <typename E>
-LinkedStack<E>::LinkedStack() : __top(NULL), __count(0) {}
+LinkedStack<E>::LinkedStack() : 
+	__top(nullptr), __count(0) {}
 
-// 소멸자 : 스택 삭제 -- 모든 노드 삭제
 template <typename E>
 LinkedStack<E>::~LinkedStack() {
 	SNode<E>* temp = __top;
@@ -69,8 +74,8 @@ LinkedStack<E>::~LinkedStack() {
 // push : 스택의 데이터 삽입
 template <typename E>
 void	LinkedStack<E>::push(const E& e) {
-	SNode<E>* newSNode = new SNode<int>;
-	newSNode->__data = e;
+	SNode<E>* newSNode = new SNode<int>(e);
+	// newSNode->__data = e;
 	newSNode->__link = __top;
 	__top = newSNode;
 	__count++;
@@ -90,23 +95,18 @@ void	LinkedStack<E>::pop(void) {
 
 // top(peek) : 스택 맨 위의 원소 확인
 template <typename E>
-E&		LinkedStack<E>::top(void) const {
-	if (empty()) error("STACK IS EMPTY");		// throw "ERROR::STACK IS EMPTY";
+E& LinkedStack<E>::top(void) const {
+	if (empty()) error("STACK IS EMPTY");			// throw "ERROR::STACK IS EMPTY";
 	return  __top->__data;
 }
 
 // empty : 스택의 공백 상태
 template <typename E>
-bool	LinkedStack<E>::empty(void) const {
-	// return !__count;
-	return __top == NULL;
-}
+bool	LinkedStack<E>::empty(void) const {	return __top == nullptr;	}
 
 // size : 스택의 크기
 template <typename E>
-int		LinkedStack<E>::size(void) const {
-	return __count;
-}
+int	LinkedStack<E>::size(void) const { return __count; }
 
 // 스택의 전체 원소 출력
 template <typename E>
