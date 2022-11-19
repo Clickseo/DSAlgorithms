2be50eb11261c288ff5f23c8c643f9f8771501c7
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 02:40:17 +0900

    Update 05_LinkedStack.c

--- c/05_LinkedStack.c
+++ c/05_LinkedStack.c
@@ -1,18 +1,18 @@
 /*
 	스택: 알고리즘 구현(C) -- 단순연결리스트
 		파일명: LinkedStack.c
-			- 스택 생성.소멸       : stackCreate, stackDestroy
-			- 데이터 삽입.삭제     : push, pop
-			- 데이터 확인(peek)    : top
-			- 빈 스택 여부 판단     : stackEempty
-			- 스택 크기            : stackSize
-			- 스택의 전체 원소 출력 : printStack
+			- 스택 생성.소멸		: stackCreate, stackDestroy
+			- 데이터 삽입.삭제		: push, pop
+			- 데이터 확인(peek)		: top
+			- 빈 스택 여부 판단		: stackEempty
+			- 스택 크기				: stackSize
+			- 스택의 전체 원소 출력	: printStack
 */
 
 #include <stdio.h>
-#include <stdlib.h>		// malloc, free
-#include "LinkedStack.h"	// LinkedStack, SNode
-// #include "LinkedNode.h"	// SNode
+#include <stdlib.h>			// malloc, free
+#include "LinkedStack.h"		// LinkedStack, SNode
+// #include "LinkedNode.h"		// SNode, DNode
 
 // stackCreate : 빈 스택 생성
 LinkedStack*	stackCreate(void) {
@@ -57,7 +57,7 @@ void	pop(LinkedStack* S) {
 
 // top(peek) : 스택 맨 위의 원소 확인
 element	top(LinkedStack* S) {
-	if (stackEempty(S))	return EOF;
+	if (stackEempty(S))	return EOF;		// return NULL;
 	return  S->__top->__data;
 }
 
@@ -67,7 +67,7 @@ _Bool	stackEempty(LinkedStack* S) {
 }
 
 // stackSize: 스택의 크기
-int	stackSize(LinkedStack* S) {
+int		stackSize(LinkedStack* S) {
 	return S->__count;
 
 }
