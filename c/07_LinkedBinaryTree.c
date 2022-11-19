19b4a3d6e9fd75ac1147af664cbeebfc0cc0f4f1
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 03:01:28 +0900

    Update 07_LinkedBinaryTree.c

--- c/07_LinkedBinaryTree.c
+++ c/07_LinkedBinaryTree.c
@@ -4,16 +4,16 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>				// malloc
-#include "LinkedStack.h"		// LinkedStack
-#include "LinkedQueue.h"		// LinkedQueue
+#include <stdlib.h>		// malloc
+#include "LinkedStack.h"	// LinkedStack
+#include "LinkedQueue.h"	// LinkedQueue
 #include "LinkedBinaryTree.h"	// DNode
-// #include "LinkedNode.h"		// SNode, DNode
-#include "Operators.h"			// isOperator,precedence, isLegal
+// #include "LinkedNode.h"	// SNode, DNode
+#include "Operators.h"		// isOperator,precedence, isLegal
 
 // 이진 트리 생성
 DNode* makeLinkedBinaryTree(char* pStr) {
-	DNode*			temp;
+	DNode*		temp;
 	LinkedStack*	S = stackCreate();
 	while (*pStr) {
 		// 공백 제거
@@ -30,7 +30,7 @@ DNode* makeLinkedBinaryTree(char* pStr) {
 		pStr++;
 	}
 	// 루트 노드
-	temp = (DNode*)top(S);		pop(S);
+	temp = (DNode*)top(S);	pop(S);
 
 	stackDestroy(S);
 	return  temp;
@@ -65,7 +65,7 @@ void  Postorder(DNode* root) {
 
 // 이진 트리 순회: 너비 우선 순회
 void  Levelorder(DNode* root) {
-	DNode*			temp;
+	DNode*		temp;
 	LinkedQueue*	Q = queueCreate();
 
 	enQueue(Q, (int)root);
