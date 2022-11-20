c3c7250043000494e256bdbeb0aeffd94b69ecce
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-20 14:08:02 +0900

    Update 06_LinkedQueue(demo).py

--- python/06_LinkedQueue(demo).py
+++ python/06_LinkedQueue(demo).py
@@ -7,7 +7,7 @@
 from LinkedQueue import LinkedQueue
 
 if __name__ == '__main__':
-    LQ = LinkedQueue()
+    Q = LinkedQueue()
     while(True):
         print('\n### 큐 구현: 단순 연결 리스트 ###')
         print('1) 데이터 삽입(push, enQueue)')
@@ -16,8 +16,8 @@ if __name__ == '__main__':
         print('4) 프로그램 종료')
         choice = int(input('메뉴 선택: '))
 
-        if choice == 1: num = int(input('삽입 할 데이터 : '));      LQ.push(num)
-        elif choice == 2: print(f'삭제 된 데이터: {LQ.front()}');   LQ.pop()
-        elif choice == 3: LQ.printQueue()
+        if choice == 1: num = int(input('삽입 할 데이터 : '));      Q.push(num)
+        elif choice == 2: print(f'삭제 된 데이터: {Q.front()}');   Q.pop()
+        elif choice == 3: Q.printQueue()
         elif choice == 4: print('프로그램 종료...');   break
         else: print('잘못 선택 하셨습니다.')
