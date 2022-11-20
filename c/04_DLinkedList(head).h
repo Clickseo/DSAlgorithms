973d05214c5bad4c32720cff2b81822014cdb57c
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-21 03:23:09 +0900

    Update 04_DLinkedList(head).h

--- c/04_DLinkedList(head).h
+++ c/04_DLinkedList(head).h
@@ -3,10 +3,10 @@
 		파일명: DLinkedList(head).h
 			구조체: DLinkedList
 				- 스택 생성.소멸		: dListCreate, dListDestroy
-				- 데이터 삽입.삭제		: dListAddRear, dListRemoveFront
-				- 데이터 확인(탐색)		: frontDNode, rearDNode
+				- 데이터 삽입.삭제	: dListAddRear, dListRemoveFront
+				- 데이터 확인(탐색)	: frontDNode, rearDNode
 				- 빈 리스트 여부 판단	: dListEmpty
-				- 리스트의 총 개수		: countDNode
+				- 리스트의 총 개수	: countDNode
 				- 리스트의 전체 출력	: printDLinkedList, revPrintDLinkedList
 */
 
@@ -17,9 +17,9 @@
 #ifndef __doubleLinkedList_H__
 #define __doubleLinkedList_H__
 typedef struct __DLinkedList {
-	DNode*		__head;	  		// 첫 번째 노드
-	// SNode*	__tail;	  		// 맨 마지막 노드
-	// int		__count;		// 노드의 총 개수
+	DNode*		__head;	  	// 첫 번째 노드
+	// SNode*	__tail;	  	// 맨 마지막 노드
+	// int		__count;	// 노드의 총 개수
 }DLinkedList;
 #endif
 
@@ -27,10 +27,10 @@ typedef struct __DLinkedList {
 DLinkedList* dListCreate(void);
 DLinkedList* dListDestroy(DLinkedList* dList);
 void    dListAddRear(DLinkedList* dList, DNode* nNode);		// 삽입: 리스트의 맨 마지막 노드로...
-void	dListRemoveFront(DLinkedList* dList);				// 삭제: 리스트에서 첫 번째 노드를...
-DNode*	frontDNode(DLinkedList* dList);						// 탐색: 리스트의 첫 번째 노드(__head)
-DNode*	rearDNode(DLinkedList* dList);						// 탐색: 리스트의 맨 마지막 노드
-int		countDNode(DLinkedList* dList);						// 탐색: 노드의 총 개수(__count)
-_Bool	dListEmpty(DLinkedList* dList);						// 빈 리스트 여부 판단
-void	printDLinkedList(DLinkedList* dList);				// 리스트의 전체 노드 출력(순방향)
-void	revPrintDLinkedList(DLinkedList* dList);			// 리스트의 전체 노드 출력(역방향)
+void	dListRemoveFront(DLinkedList* dList);			// 삭제: 리스트에서 첫 번째 노드를...
+DNode*	frontDNode(DLinkedList* dList);				// 탐색: 리스트의 첫 번째 노드(__head)
+DNode*	rearDNode(DLinkedList* dList);				// 탐색: 리스트의 맨 마지막 노드
+int	countDNode(DLinkedList* dList);				// 탐색: 노드의 총 개수(__count)
+_Bool	dListEmpty(DLinkedList* dList);				// 빈 리스트 여부 판단
+void	printDLinkedList(DLinkedList* dList);			// 리스트의 전체 노드 출력(순방향)
+void	revPrintDLinkedList(DLinkedList* dList);		// 리스트의 전체 노드 출력(역방향)
