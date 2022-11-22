cb9005433a36b106da11b4354fc820dc8add7ef1
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-22 15:11:22 +0900

    Update 05_LinkedStack(demo).cpp

--- cpp/05_LinkedStack(demo).cpp
+++ cpp/05_LinkedStack(demo).cpp
@@ -9,9 +9,8 @@ using namespace std;
 
 int main(void)
 {
-	LinkedStack<int>	LS;
 	int			num, choice;
-
+	LinkedStack<int>	S;
 	while (true) {
 		system("cls");
 		cout << "\n ### 스택 구현: 단순 연결 리스트 ### \n" << endl;
@@ -24,16 +23,19 @@ int main(void)
 
 		switch (choice) {
 			case 1: cout << "\n삽입 할 데이터 입력: ";
-				cin >> num;	LS.push(num);	break;
-			case 2: cout << "삭제 된 데이터: " << LS.top() << endl;
-				LS.pop();	break;
-			case 3: LS.printStack();
+				cin >> num;
+				S.push(num);
+				break;
+			case 2: cout << "삭제 된 데이터: " << S.top() << endl;
+				S.pop();
+				break;
+			case 3: S.printStack();
 				break;
 			case 4: cout << "프로그램 종료..." << endl;
 				return 0;
 			default: cout << "잘못 선택 하셨습니다." << endl;
 		}
 		system("pause");
-	}	
+	}
 	return 0;	// s.~ListStack();
 }
