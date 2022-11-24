7deeada69de1a285cf842390f795ecb6308645d2
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-24 09:52:56 +0900

    Create 10_adjSList.cpp

---
+++ cpp/10_adjSList.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+// GNode class
+class GNode {
+private:
+	int	__vertex;	// 정점
+	int	__weight;	// 가중치
+	GNode*	__link;
+	friend class GraphType;
+public:
+	GNode(int vertex, int weight);
+};
+
+// 그래프 노드(C): 그래프 노드 생성
+GNode::GNode(int vertex, int weight)
+	: __vertex(vertex), __weight(weight), __link(nullptr) {}
+
+// GraphType class
+class GraphType {
+private:
+	int	 __vertex;	// 정점의 개수
+	GNode**	 __adjMatrix;	// 인접 리스트
+public:
+	GraphType(int vertex);
+	~GraphType(void);
+	void	insertEdge(int vertex1, int vertex2, int weight);
+	void	printAdjMatrix(void) const;
+};
+
+int main(void)
+{
+	// G2 : 무향 그래프
+	GraphType	G2 = GraphType(3);
+
+	// 정점: A(0)
+	G2.insertEdge(0, 1, 0);	// A(0) - B(1)
+	G2.insertEdge(0, 2, 0);	// A(0) - C(2)
+
+	// 정점: B(1)
+	G2.insertEdge(1, 0, 0);	// B(1) - A(0)
+	G2.insertEdge(1, 2, 0);	// B(1) - C(2)
+
+	// 정점: C(2)
+	G2.insertEdge(2, 1, 0);	// C(2) - B(1)
+	G2.insertEdge(2, 0, 0);	// C(2) - A(0)
+
+	cout << "\n##### 그래프(G2): 인접 행렬 #####" << endl;
+	G2.printAdjMatrix();
+
+	// G4 : 유향 그래프
+	GraphType	G4 = GraphType(3);
+
+	// 정점: A(0)
+	G4.insertEdge(0, 1, 0);	// A(0) - B(1)
+	G4.insertEdge(0, 2, 0);	// A(0) - C(2)
+
+	// 정점: B(1)
+	G4.insertEdge(1, 0, 0);	// B(1) - A(0)
+	G4.insertEdge(1, 2, 0);	// B(1) - C(2)
+
+	cout << "\n##### 그래프(G4): 인접 행렬 #####" << endl;
+	G4.printAdjMatrix();
+
+	return 0;
+}
