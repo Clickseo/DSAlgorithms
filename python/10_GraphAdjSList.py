cd5ee79b8ccbf95da2c182c0034aa665227022f7
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-29 03:12:12 +0900

    Update 10_GraphAdjSList.py

--- python/10_GraphAdjSList.py
+++ python/10_GraphAdjSList.py
@@ -1,12 +1,12 @@
 '''
 	그래프 표현(인접 리스트): 알고리즘 구현
 		파일명: GraphAdjSList.py
-			- main	: 그래프 생성 및 간선 추가
-			클래스	: GNode
-			클래스	: GraphType
-				- 그래프 생성	: __init__
-				- 간선 추가	: insertEdge
-				- 전체 출력	: printAdjMatrix
+			- __main__	: 그래프 생성 및 간선 추가
+			- 클래스    : GNode
+			- 클래스    : GraphType
+                        그래프 생성.소멸      : __init__, __del__
+                        그래프 간선 추가		: insertEdge
+                        그래프 전체 출력		: printAdjSList
 '''
 
 # GNode class
@@ -19,11 +19,20 @@ class GNode :
 # GraphType class
 class GraphType :
     def __init__(self, vertex: int) :
-        self.__vertex = vertex    # 정점의 개수
-        self.__adjSList = []     # 인접 리스트
-        for i in range(self.__vertex) :           
-           self.__adjSList.append(GNode(0, 0))
+        self.__vertex = vertex  # 정점의 개수
+        self.__adjSList = []    # 인접 리스트
+        for i in range(self.__vertex) :
+           self.__adjSList.append(None)
 
+    def __del__(self):
+        for i in range(len(self.__adjSList)) :
+            temp = self.__adjSList[i]
+            while temp :
+                self.__adjSList[i] = temp.link
+                del temp
+                temp = self.__adjSList[i]
+        del self
+        
     # insertEdge : 간선 추가
     def insertEdge(self, row:int, col:int, weight:int) :
         if row >= self.__vertex or col >= self.__vertex :
@@ -43,7 +52,7 @@ class GraphType :
             print(f'\t정점 {chr(i+65)}의 인접 리스트: ', end='')
             tNode = self.__adjSList[i]
             while tNode :
-                print(f'{chr(tNode.vertex + 65):3}', end='')
+                print(f'{chr(tNode.vertex + 65)} -> ', end='')
                 tNode = tNode.link
             print(' NULL')
 
@@ -77,6 +86,6 @@ if __name__ == '__main__' :
     G4.insertEdge(1, 0, 0)	# B(1) - A(0)
     G4.insertEdge(1, 2, 0)	# B(1) - C(2)
 
-    print(f'\n##### 그래프(G4): 인접 리스트 #####\n');
+    print('\n##### 그래프(G4): 인접 리스트 #####\n');
     G4.printAdjSList()
 
