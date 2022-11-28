50af3cb46b2eba5946a8f2bf0a4ce5e9afb92643
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-29 00:17:16 +0900

    Update 10_GraphAdjSList.c

--- c/10_GraphAdjSList.c
+++ c/10_GraphAdjSList.c
@@ -1,12 +1,12 @@
 /*
 	그래프 표현(인접 리스트): 알고리즘 구현
 		파일명: GraphAdjSList.c
-			- main	: 그래프 생성 및 정점 추가
-			- 그래프 생성 및 활용: graphCreate, graphDestroy, insertEdge, printAdjMatrix
+			- main	: 그래프 생성 및 간선 추가
+			- 그래프 생성 및 활용: graphCreate, graphDestroy, insertEdge, printAdjSList
 */
 
 #include <stdio.h>
-#include <stdlib.h>	// malloc, calloc, free
+#include <stdlib.h>		// malloc, calloc, free
 
 // 그래프(노드): GNode
 typedef struct __GNode {
@@ -21,14 +21,14 @@ GNode* makeGNode(int vertex, int weight);
 // 그래프: GraphType
 typedef  struct  __GraphType {
 	int	__vertex;	// 정점의 개수
-	GNode** __adjMatrix;	// 인접 리스트
+	GNode** __adjSList;	// 인접 리스트
 } GraphType;
 
 // 그래프 구현(C): 그래프 생성 및 활용
 GraphType* graphCreate(int vertex);
-void	graphDestroy(GraphType* G);
+GraphType* graphDestroy(GraphType* G);
 void	insertEdge(GraphType* G, int vertex1, int vertex2, int weight);
-void	printAdjMatrix(GraphType* G);
+void	printAdjSList(GraphType* G);
 
 int main(void)
 {
@@ -47,8 +47,8 @@ int main(void)
 	insertEdge(G2, 2, 1, 0);	// C(2) - B(1)
 	insertEdge(G2, 2, 0, 0);	// C(2) - A(0)
 
-	printf("\n##### 그래프(G2): 인접 리스트 #####\n");
-	printAdjMatrix(G2);
+	printf("\n##### 그래프(G2): 인접 리스트 #####\n\n");
+	printAdjSList(G2);
 	graphDestroy(G2);
 
 	// G4 : 유향 그래프
@@ -62,8 +62,8 @@ int main(void)
 	insertEdge(G4, 1, 0, 0);	// B(1) - A(0)
 	insertEdge(G4, 1, 2, 0);	// B(1) - C(2)
 
-	printf("\n##### 그래프(G4): 인접 리스트 #####\n");
-	printAdjMatrix(G4);
+	printf("\n##### 그래프(G4): 인접 리스트 #####\n\n");
+	printAdjSList(G4);
 	graphDestroy(G4);
 
 	return 0;
@@ -90,21 +90,24 @@ GraphType* graphCreate(int vertex) {
 		exit(1);
 	}
 	G->__vertex = vertex;
-	G->__adjMatrix = (GNode**)calloc(vertex + 1, sizeof(GNode*));
+	G->__adjSList = (GNode**)calloc(vertex + 1, sizeof(GNode*));
 	return G;
 }
 
 // graphDestroy : 그래프 삭제
-void	graphDestroy(GraphType* G) {
+GraphType* graphDestroy(GraphType* G) {
 	GNode* old;
 	for (int i = 0; i < G->__vertex; i++) {
-		old = G->__adjMatrix[i];
+		old = G->__adjSList[i];
 		while (old) {
-			G->__adjMatrix[i] = old->__link;
+			G->__adjSList[i] = old->__link;
 			free(old);
-		}		
+			old = G->__adjSList[i];
+		}
 	}
-	G->__vertex = 0;
+	free(G->__adjSList);
+	free(G);
+	return NULL;
 }
 
 // insertEdge : 간선 추가
@@ -115,10 +118,10 @@ void  insertEdge(GraphType* G, int  row, int  col, int weight) {
 	}
 
 	GNode* newGNode = makeGNode(col, weight);
-	if (G->__adjMatrix[row] == NULL)
-		G->__adjMatrix[row] = newGNode;
+	if (G->__adjSList[row] == NULL)
+		G->__adjSList[row] = newGNode;
 	else {
-		GNode* rNode = G->__adjMatrix[row];
+		GNode* rNode = G->__adjSList[row];
 		while (rNode->__link)
 			rNode = rNode->__link;
 		rNode->__link = newGNode;
@@ -126,19 +129,19 @@ void  insertEdge(GraphType* G, int  row, int  col, int weight) {
 }
 
 // printAdjMatrix : 그래프 전체 출력
-void  printAdjMatrix(GraphType* G) {
-	if (G->__adjMatrix == NULL) {
+void  printAdjSList(GraphType* G) {
+	if (G->__adjSList == NULL) {
 		printf("그래프의 정점이 %d 입니다.\n", G->__vertex);
 		return;
 	}
 
 	for (int i = 0; i < G->__vertex; i++) {
-		printf("\n\t정점 %c의 인접 리스트: ", i + 65);
-		GNode* tNode = G->__adjMatrix[i];
+		printf("\t정점 %c의 인접 리스트: ", i + 65);
+		GNode* tNode = G->__adjSList[i];
 		while (tNode) {
-			printf("%3c ->>", tNode->__vertex + 65);
+			printf("%3c ->", tNode->__vertex + 65);
 			tNode = tNode->__link;
 		}
-	}
-	printf(" NULL\n");
+		printf(" NULL\n");
+	}	
 }
