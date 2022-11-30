622419bddb7e90190e144fb5e076241abbc11052
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-11-30 18:27:03 +0900

    Update 10_GraphAdjMatrix.py

--- python/10_GraphAdjMatrix.py
+++ python/10_GraphAdjMatrix.py
@@ -2,14 +2,15 @@
 	그래프 표현(인접 행렬): 알고리즘 구현
 		파일명: GraphAdjMatrix.py
 			- main	: 그래프 생성 및 간선 추가
-			클래스	: GraphType
-				- 그래프 생성	: __init__
-				- 간선 추가	: insertEdge
-				- 전체 출력	: printAdjMatrix
+			- 클래스	: GraphType
+				그래프 생성 : __init__
+				그래프 간선 추가 : insertEdge
+				그래프 전체 출력 : printAdjMatrix
 '''
 
 # GraphType class
 class GraphType :
+    # 생성자: 그래프 생성
     def __init__(self, vertex: int) : 
         self.__vertex = vertex    # 정점의 개수
         self.__adjMatrix = []     # 인접 행렬
@@ -19,14 +20,14 @@ class GraphType :
                 s.append(0);
             self.__adjMatrix.append(s)
 
-    # insertEdge : 간선 추가
+    # 그래프 간선 추가
     def insertEdge(self, row: int, col: int, weight: int) :
         if row >= self.__vertex or col >= self.__vertex :
             print('그래프에 없는 정점입니다!!!')
             return
         self.__adjMatrix[row][col] = weight
         
-    # printAdjMatrix : 그래프 전체 출력
+    # 그래프 전체 출력
     def printAdjMatrix(self) :
         for i in range(self.__vertex) :
             print('\t', end='')
