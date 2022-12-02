f9b1e44d9dd4910e99cd117c1efe1ef128f008f3
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-12-02 14:19:44 +0900

    Update 04_SLinkedList(head).c

--- c/04_SLinkedList(head).c
+++ c/04_SLinkedList(head).c
@@ -1,9 +1,10 @@
 /*
 	단순 연결 리스트: 알고리즘 구현(C)
 		파일명: SLinkedList(head).c
+		함수원형: 
 			- 리스트 성생 및 삭제	: sListCreate, sListDestroy
-			- 노드 탐색		: sListEmpty, countSNode, frontSNode, rearSNode
 			- 노드 삽입 및 삭제	: sListAddRear, sListRemoveFront
+			- 노드 탐색		: sListEmpty, countSNode, frontSNode, rearSNode
 			- 전체 원소 출력	: printSLinkedList
 */
 
@@ -24,7 +25,7 @@ SLinkedList* sListCreate(void) {
 	return sList;
 }
 
-// 리스트 삭제: 리스트의 전체 노드 삭제
+// 리스트 삭제: 전체 노드 삭제
 SLinkedList* sListDestroy(SLinkedList* sList) {
 	//	while (!sListEmpty(sList))
 	//		sListRemoveFront(sList);
@@ -35,23 +36,8 @@ SLinkedList* sListDestroy(SLinkedList* sList) {
 		tNode = tNode->__link;
 		free(old);
 	}
+	
 	return NULL;
-}
-
-// 탐색: 첫 번째 노드
-SNode* frontSNode(SLinkedList* sList) {
-	return sList->__head;
-}
-
-// 탐색: 맨 마지막 노드(tail)
-SNode* rearSNode(SLinkedList* sList) {
-	if (sListEmpty(sList))
-		return NULL;
-
-	SNode* rNode = sList->__head;
-	while (rNode->__link)
-		rNode = rNode->__link;
-	return rNode;
 }
 
 // 노드 삽입: 맨 마지막 노드로...
@@ -73,6 +59,22 @@ void	sListRemoveFront(SLinkedList* sList) {
 	SNode* old = sList->__head;
 	sList->__head = old->__link;
 	free(old);
+}
+
+// 탐색: 첫 번째 노드(head)
+SNode* frontSNode(SLinkedList* sList) {
+	return sList->__head;
+}
+
+// 탐색: 맨 마지막 노드(tail)
+SNode* rearSNode(SLinkedList* sList) {
+	if (sListEmpty(sList))
+		return NULL;
+
+	SNode* rNode = sList->__head;
+	while (rNode->__link)
+		rNode = rNode->__link;
+	return rNode;
 }
 
 // 빈 리스트 여부 판단
