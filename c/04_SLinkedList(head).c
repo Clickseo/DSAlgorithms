5102c86b3225239703a2d80ae00a83649cbf6418
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-12-02 14:31:24 +0900

    Update 04_SLinkedList(head).c

--- c/04_SLinkedList(head).c
+++ c/04_SLinkedList(head).c
@@ -3,9 +3,9 @@
 		파일명: SLinkedList(head).c
 		함수원형: 
 			- 리스트 성생 및 삭제	: sListCreate, sListDestroy
-			- 노드 삽입 및 삭제	: sListAddRear, sListRemoveFront
-			- 노드 탐색		: sListEmpty, countSNode, frontSNode, rearSNode
-			- 전체 원소 출력	: printSLinkedList
+			- 노드 삽입.삭제	: sListAddRear, sListRemoveFront
+			- 노드 검색		: sListEmpty, countSNode, frontSNode, rearSNode
+			- 리스트 전체 출력	: printSLinkedList
 */
 
 #include <stdio.h>
@@ -61,12 +61,12 @@ void	sListRemoveFront(SLinkedList* sList) {
 	free(old);
 }
 
-// 탐색: 첫 번째 노드(head)
+// 검색: 첫 번째 노드(head)
 SNode* frontSNode(SLinkedList* sList) {
 	return sList->__head;
 }
 
-// 탐색: 맨 마지막 노드(tail)
+// 검색: 맨 마지막 노드(tail)
 SNode* rearSNode(SLinkedList* sList) {
 	if (sListEmpty(sList))
 		return NULL;
@@ -77,12 +77,7 @@ SNode* rearSNode(SLinkedList* sList) {
 	return rNode;
 }
 
-// 빈 리스트 여부 판단
-_Bool	sListEmpty(SLinkedList* sList) {
-	return sList->__head == NULL;
-}
-
-// 탐색: 노드의 총 개수(count)
+// 검색: 노드의 총 개수(count)
 int	countSNode(SLinkedList* sList) {
 	if (sListEmpty(sList))
 		return 0;
@@ -96,7 +91,12 @@ int	countSNode(SLinkedList* sList) {
 	return count;
 }
 
-// 리스트의 전체 노드 출력
+// 빈 리스트 여부 판단
+_Bool	sListEmpty(SLinkedList* sList) {
+	return sList->__head == NULL;
+}
+
+// 리스트 전체 출력
 void	printSLinkedList(SLinkedList* sList) {
 	if (sListEmpty(sList)) {
 		printf("입력된 데이터가 없습니다... \n");
