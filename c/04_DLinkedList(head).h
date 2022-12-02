322d55b3afeaa976a008336f5c12dc2c2356a60c
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2022-12-02 14:32:34 +0900

    Update 04_DLinkedList(head).h

--- c/04_DLinkedList(head).h
+++ c/04_DLinkedList(head).h
@@ -5,9 +5,9 @@
 			함수원형:			
 				- 스택 생성.소멸	: dListCreate, dListDestroy
 				- 데이터 삽입.삭제	: dListAddRear, dListRemoveFront
-				- 데이터 확인(탐색)	: frontDNode, rearDNode
-				- 빈 리스트 여부 판단	: dListEmpty
+				- 노드 검색	: frontDNode, rearDNode
 				- 리스트의 총 개수	: countDNode
+				- 빈 리스트 여부 판단	: dListEmpty				
 				- 리스트의 전체 출력	: printDLinkedList, revPrintDLinkedList
 */
 
@@ -29,9 +29,9 @@ DLinkedList* dListCreate(void);
 DLinkedList* dListDestroy(DLinkedList* dList);
 void	dListAddRear(DLinkedList* dList, DNode* nNode);		// 삽입: 리스트의 맨 마지막 노드로...
 void	dListRemoveFront(DLinkedList* dList);			// 삭제: 리스트에서 첫 번째 노드를...
-DNode*	frontDNode(DLinkedList* dList);				// 탐색: 리스트의 첫 번째 노드(head)
-DNode*	rearDNode(DLinkedList* dList);				// 탐색: 리스트의 맨 마지막 노드(tail)
-int	countDNode(DLinkedList* dList);				// 탐색: 노드의 총 개수(count)
+DNode*	frontDNode(DLinkedList* dList);				// 검색: 첫 번째 노드(head)
+DNode*	rearDNode(DLinkedList* dList);				// 검색: 맨 마지막 노드(tail)
+int	countDNode(DLinkedList* dList);				// 검색: 노드의 총 개수(count)
 _Bool	dListEmpty(DLinkedList* dList);				// 빈 리스트 여부 판단
 void	printDLinkedList(DLinkedList* dList);			// 리스트의 전체 노드 출력(순방향)
 void	revPrintDLinkedList(DLinkedList* dList);		// 리스트의 전체 노드 출력(역방향)
