fc195571f5a2d6144b777472fd5b187dd33312b8
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-02 15:41:17 +0900

    Update 07_LinkedBTree.py

--- python/07_LinkedBTree.py
+++ python/07_LinkedBTree.py
@@ -2,7 +2,7 @@
 	이진 트리: 알고리즘 구현
         파일명: LinkedBTree.py
         		- main: 데이터 삽입.삭제, 전체 원소 출력
-			클래스: TreeNode
+			클래스: DNode
 			클래스: LinkedBinaryTree
 				- 이진 트리 생성                    : makeLinkedBinaryTree
 				- 깊이 우선 순회(전위.중위.후위 순회) : Preorder, Inorder, Postorder
@@ -21,13 +21,18 @@ class DNode :
 
 class LinkedBTree :
     def __init__(self):
-        self.__root = None    
+        self.__root = None
+	    
     # 이진 트리(수식 트리) 생성: 스택 구조 활용    
     def makeLinkedBTree(self, postfix) -> DNode:
-    def Preorder(self):    # 깊이 우선 순회: 전위 순회    
-    def Inorder(self):      # 깊이 우선 순회: 중위 순회    
-    def Postorder(self):    # 깊이 우선 순회: 후위 순회    
-    def Levelorder(self):    # 너비 우선 순회: 비재귀적 용법(큐 구조 활용)
+	    
+    # 깊이 우선 순회: 전위.중위.후위 순회    
+    def Preorder(self):
+    def Inorder(self):
+    def Postorder(self):
+	    
+    # 너비 우선 순회: 비재귀적 용법(큐 구조 활용)
+    def Levelorder(self):
     def __del__(self):      # 소멸자: 이진 트리의 모든 노드 삭제
       
 # from LinkedBTree import LinkedBTree
@@ -42,5 +47,5 @@ if __name__ == '__main__':
     print('Inorder   : ', end=' ');     BT.Inorder();     print('')
     print('Postorder : ', end=' ');     BT.Postorder();   print('')
 
-	  # 이진 트리 너비 우선 순회
+    # 이진 트리 너비 우선 순회
     print('Levelorder : ', end=' ');    BT.Levelorder();  print('')
