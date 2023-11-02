29110c9f9b55fc103e0b3d116585b4e1f141c5f5
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-02 18:42:30 +0900

    Update 07_LinkedBTree(demo).cpp

--- cpp/07_LinkedBTree(demo).cpp
+++ cpp/07_LinkedBTree(demo).cpp
@@ -1,7 +1,54 @@
 /*
+	파일명: LinkedBtree.h
+ 	클래스 설계 및 구현: DNode, LinkedBTree
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// 연산자 여부를 판단
+int  isOperator(int  op) {
+	return op == '+' || op == '-' || op == '*' || op == '/';
+}
+
+template <typename E>
+class LinkedBTree;
+
+// class DNode
+template <typename E>
+class DNode {
+public:
+	DNode(const E& data);
+private:
+	E         __data;
+	DNode<E>* __Llink;
+	DNode<E>* __Rlink;
+	template <typename E> friend class LinkedBTree;
+};
+
+// class LinkedBTree
+template <typename E>
+class LinkedBTree {
+public:
+	LinkedBTree();
+	~LinkedBTree();
+	DNode<E>* makeLinkedBTree(const string str);
+	void	Preorder(DNode<E>* root) const;
+	void	Inorder(DNode<E>* root) const;
+	void	Postorder(DNode<E>* root) const;
+	void	Levelorder(DNode<E>* root) const;
+private:
+	DNode<E>* __root;
+};
+// DNode, LinkedBTree Class 멤버 함수 정의 생략
+*/
+
+/*
 	이진 트리: 알고리즘 구현 -- 단순연결리스트
 		파일명: LinkedBTree(demo).cpp
-			- main: 이진 트리 구성과 순회
+			- main: 이진 트리(수식 트리) 구성 및 순회
 */
 
 #include <iostream>
@@ -28,7 +75,6 @@ int main(void)
 
 	// 너비 우선 순회
 	cout << "\nLevelorder : ";	BT.Levelorder(root);
-	cout << endl;
 
 	return 0;
 }
