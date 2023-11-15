b192fd300b3358b9145bcd4583e594d90a563db6
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-15 16:55:00 +0900

    Update 08_LinkedBSTree.h

--- c/08_LinkedBSTree.h
+++ c/08_LinkedBSTree.h
@@ -5,9 +5,8 @@
 			- 깊이 우선 순회: 전위 순회(재귀적 용법): Preorder
 */
 
-// 이중 연결 리스트 구현(C)
-// 노드: DNode(data, Llink, Rlink)
 // #pragma once
+// 노드: DNode(data, Llink, Rlink)
 #include "LinkedNode.h"		// DNode
 
 // 이진 검색 트리(BST) 구현
