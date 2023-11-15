58b13937c8ddd9e8366726003378f7724355b839
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-15 17:41:51 +0900

    Create 08_LinkedBSTree.h

---
+++ c/08_LinkedBSTree.h
@@ -0,0 +1,16 @@
+/*
+	이진 검색 트리(BST): 알고리즘 구현(C) -- 이중연결리스트
+		파일명: LinkedBSTree.h
+			- 데이터 검색, 삽입, 삭제: searchBST, insertBST, deleteBST
+			- 깊이 우선 순회: 전위 순회 -- Preorder
+*/
+
+// #pragma once
+// 노드: DNode(data, Llink, Rlink)
+#include "LinkedNode.h"		// DNode
+
+// 이진 검색 트리(BST) 구현
+void	Preorder(DNode* root);		// 전체 원소 출력: 전위 순회
+DNode	*searchBST(DNode* root, element data);	// 데이터 검색
+DNode	*insertBST(DNode *root, element data);	// 데이터 삽입
+DNode	*deleteBST(DNode *root, element data);	// 데이터 삭제
