97c68cd55b7a3130720850d21378a464190d7ec7
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-21 14:47:32 +0900

    Update 08_AVLTree.py

--- python/08_AVLTree.py
+++ python/08_AVLTree.py
@@ -1,32 +1,28 @@
 class AVLNode:
-	def __init__(self, newItem, left, right, h):
- 		self.item = newItem
- 		self.left = left
- 		self.right = right
- 		self.height = h
+    def __init__(self, newItem, left, right, h):
+        self.item = newItem
+        self.left = left
+        self.right = right
+        self.height = h
 
 class AVLTree:
     def __init__(self):
         self.NIL = AVLNode(None, None, None, 0)
         self.__root = self.NIL
         self.LL = 1; self.LR = 2; self.RR = 3; self.RL = 4
-        self.NO_NEED = 0;
+        self.NO_NEED = 0
         self.ILLEGAL = -1
-        
-    def search(x):
-        return __searchItem(self.__root, x)
+
+    def search(self, x):
+        return self.__searchItem(self.__root, x)        
     def __searchItem(self, tNode:AVLNode, x) -> AVLNode:
-        if tNode == self.NIL:
-            return self.NIL
-        elif x == tNode.item:
-            return tNode
-        elif x < tNode.item:
-            return __searchItem(tNode.left, x)
-        else:
-            return __searchItem(tNode.right, x)
-  
+        if tNode == self.NIL: return self.NIL
+        elif x == tNode.item: return tNode
+        elif x < tNode.item: return self.__searchItem(tNode.left, x)
+        else :               return self.__searchItem(tNode.right, x)
+      
     def insert(self, x):
-        self.__root = self.__insertItem(self.__root, x)  
+        self.__root = self.__insertItem(self.__root, x)
     def __insertItem(self, tNode:AVLNode, x) -> AVLNode:
         if tNode == self.NIL: # insert after a leaf(or into an empty tree)
             tNode = AVLNode(x, self.NIL, self.NIL, 1)
@@ -36,7 +32,7 @@ class AVLTree:
             type = self.__needBalance(tNode)
             if (type != self.NO_NEED):
                 tNode = self.__balanceAVL(tNode, type)
-        else: # branch right
+        else : # branch right
             tNode.right = self.__insertItem(tNode.right, x)
             tNode.height = 1 + max(tNode.right.height, tNode.left.height)
             type = self.__needBalance(tNode)
@@ -47,18 +43,16 @@ class AVLTree:
     def delete(self, x):
         self.__root = self.__deleteItem(self.__root, x)   
     def __deleteItem(self, tNode:AVLNode, x) -> AVLNode:
-        if tNode == self.NIL:
-            return self.NIL	# item not found!
-        else:
-            if x == tNode.item: # item found at tNode
-                tNode = self.__deleteNode(tNode)
+        if tNode == self.NIL: return self.NIL	# item not found!
+        else : # item found at tNode
+            if x == tNode.item: tNode = self.__deleteNode(tNode)
             elif x < tNode.item:
                 tNode.left = self.__deleteItem(tNode.left, x)
                 tNode.height = 1 + max(tNode.right.height, tNode.left.height)
                 type = self.__needBalance(tNode)
                 if type != self.NO_NEED:
                     tNode = self.__balanceAVL(tNode, type)
-            else:
+            else :
                 tNode.right = self.__deleteItem(tNode.right, x)
                 tNode.height = 1 + max(tNode.right.height, tNode.left.height)
                 type = self.__needBalance(tNode)
@@ -71,69 +65,66 @@ class AVLTree:
   		#     1. tNode이 리프 노드
   		#     2. tNode이 자식이 하나만 있음
   		#     3. tNode이 자식이 둘 있음
-        if tNode.left == self.NIL and tNode.right == self.NIL: # case 1(자식이 없음)
+        # case 1(자식이 없음)
+        if tNode.left == self.NIL and tNode.right == self.NIL:
             return self.NIL
-        elif tNode.left == self.NIL:  # case 2(오른쪽자식뿐)
-            return tNode.right
-        elif tNode.right == self.NIL: # case 2(왼쪽자식뿐)
-            return tNode.left
-        else: # case 3(두 자식이 다 있음)
+        # case 2(오른쪽 자식뿐)
+        elif tNode.left == self.NIL : return tNode.right
+        # case 2(왼쪽 자식뿐)
+        elif tNode.right == self.NIL: return tNode.left
+        # case 3(두 자식이 다 있음)
+        else :
             (rtnItem, rtnNode) = self.__deleteMinItem(tNode.right)
             tNode.item = rtnItem
             tNode.right = rtnNode
             tNode.height = 1 + max(tNode.right.height, tNode.left.height)
-            type = self.__needBalance(tNode)	
+            type = self.__needBalance(tNode)
             if type != self.NO_NEED:
                 tNode = self.__balanceAVL(tNode, type)
-            return tNode  # tNode survived
+            return tNode    # tNode survived
 
     def __deleteMinItem(self, tNode:AVLNode) -> tuple:
-        if tNode.left == self.NIL:
-            # found min at tNode
+        if tNode.left == self.NIL:  # found min at tNode
             return (tNode.item, tNode.right)
-        else: # keep branching left, then backtrack
+        else:   # keep branching left, then backtrack
             (rtnItem, rtnNode) = self.__deleteMinItem(tNode.left)
             tNode.left = rtnNode
             tNode.height = 1 + max(tNode.right.height, tNode.left.height) 
             type = self.__needBalance(tNode)
-            if type != self.NO_NEED:
-                tNode = self.__balanceAVL(tNode, type)
+            if type != self.NO_NEED: tNode = self.__balanceAVL(tNode, type)
             return (tNode, rtnItem)
  	
     def __needBalance(self, t:AVLNode) -> int:
         type = self.ILLEGAL
-        if (t.left.height + 2 <= t.right.height): # R 유형
+        if (t.left.height + 2 <= t.right.height):   # R 유형
             if (t.right.left.height) <= t.right.right.height:
                 type = self.RR
-            else:
-                type = self.RL;
+            else : type = self.RL
         elif (t.left.height) >= t.right.height + 2: # L 유형
             if (t.left.left.height) >= t.left.right.height:
                 type = self.LL
-            else:
-                type = self.LR
-        else:
-            type = self.NO_NEED
+            else : type = self.LR
+        else : type = self.NO_NEED
         return type
      
-    # 균형 잡기
+ 	# 균형 잡기
     def __balanceAVL(self, tNode:AVLNode, type:int) -> AVLNode:
         returnNode = self.NIL
-        if type == self.LL:
+        if type == self.LL :
             returnNode = self.__rightRotate(tNode)
-        elif type == self.LR:
+        elif type == self.LR :
             tNode.left = self.__leftRotate(tNode.left)
             returnNode = self.__rightRotate(tNode)
-        elif type == self.RR:
+        elif type == self.RR :
             returnNode = self.__leftRotate(tNode)
-        elif type == self.RL:
+        elif type == self.RL :
             tNode.right = self.__rightRotate(tNode.right)
             returnNode = self.__leftRotate(tNode)
         else:
             print("Impossible type! Should be one of LL, LR, RR, RL")
         return returnNode
   
-    # 좌회전
+ 	# 좌회전
     def __leftRotate(self, t:AVLNode) -> AVLNode:
         RChild = t.right
         if RChild == self.NIL:
@@ -145,7 +136,7 @@ class AVLTree:
         RChild.height = 1 + max(RChild.left.height, RChild.right.height)
         return RChild
  
-    # 우회전
+ 	# 우회전
     def __rightRotate(self, t:AVLNode) -> AVLNode:
         LChild = t.left
         if LChild == self.NIL:
@@ -163,10 +154,11 @@ class AVLTree:
  
     def clear(self):
         self.__root = self.NIL
-
-      
-if __name__ == '__main__’:
+        
+# from AVLTree import AVLTree
+if __name__ == '__main__':
     bst1 = AVLTree()
+
     bst1.insert(10)
     bst1.insert(20)
     bst1.insert(5)
