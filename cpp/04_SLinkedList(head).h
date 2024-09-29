711449b6b944126b3b229bd35179e0f1810b5875
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-09-29 22:22:55 +0900

    Update 04_SLinkedList(head).h

--- cpp/04_SLinkedList(head).h
+++ cpp/04_SLinkedList(head).h
@@ -5,7 +5,7 @@
 */
 
 // #pragma once
-#include "LinkedNode.h"			// SNode
+#include "LinkedNode.h"		// SNode
 
 #ifndef __SLinkedList_H__
 #define __SLinkedList_H__
@@ -13,15 +13,15 @@
 // SLinkedList class
 class SLinkedList {
 public:
-	SLinkedList(void);							// 생성자
-	~SLinkedList(void);							// 소멸자: 전체 노드 삭제
-	bool	isEmpty(void) const;				// 빈 리스트 여부 판단
-	int		countNode(void) const;				// 탐색: 노드의 총 개수(count_)
-	SNode*	frontNode(void) const;				// 탐색: 첫 번째 노드(head_)
-	SNode*	rearNode(void) const;				// 탐색: 맨 마지막 노드(tail_)
-	void	addRear(const int& e);				// 삽입: 맨 마지막 노드(tail_)
-	void	removeFront(void);					// 삭제: 첫 번째 노드(head_)
-	void	printLinkedList(void) const;		// 리스트의 전체 원소(노드) 출력
+	SLinkedList(void);			// 생성자
+	~SLinkedList(void);			// 소멸자: 전체 노드 삭제
+	bool	isEmpty(void) const;		// 빈 리스트 여부 판단
+	int	countNode(void) const;		// 탐색: 노드의 총 개수(count_)
+	SNode*	frontNode(void) const;		// 탐색: 첫 번째 노드(head_)
+	SNode*	rearNode(void) const;		// 탐색: 맨 마지막 노드(tail_)
+	void	addRear(const int& e);		// 삽입: 맨 마지막 노드(tail_)
+	void	removeFront(void);		// 삭제: 첫 번째 노드(head_)
+	void	printLinkedList(void) const;	// 리스트의 전체 원소(노드) 출력
 private:
 	SNode*		head_;		// 첫 번째 노드
 	// SNode*	tail_;		// 맨 마지막 노드
