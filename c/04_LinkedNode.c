4152ec256e860696ceb69b676ed6b5610536dfdc
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-09-29 22:53:05 +0900

    Update 04_LinkedNode.c

--- c/04_LinkedNode.c
+++ c/04_LinkedNode.c
@@ -1,15 +1,15 @@
 /*
-	단순.이중 연결 리스트 : 알고리즘 구현(C)
-		파일명: LinkedNode.c
-			- 노드		: SNode(data, link)
-			- 노드		: DNode(data, Llink, Rlink)
+	단순.이중 연결 리스트 : 알고리즘 구현(c)
+		파일명: LinkedNode.h
+			- 구조체 : SNode / 단순 연결 리스트(노드)	: SNode(data, link)
+			- 구조체: DNode / 이중 연결 리스트(노드)	: DNode(data, Llink, Rlink)
 */
 
 #include <stdio.h>
-#include <stdlib.h>		// malloc, free
+#include <stdlib.h>			// malloc, free
 #include "LinkedNode.h"		// SNode, DNode
 
-// 단순 연결 리스트 구현(C)
+// 단순 연결 리스트: SNode(data, link)
 // 새로운 노드(SNode: data, link) 생성
 SNode* makeSNode(element num) {
 	SNode* newNode = (SNode*)malloc(sizeof(SNode));
@@ -22,7 +22,7 @@ SNode* makeSNode(element num) {
 	return newNode;
 }
 
-// 이중 연결 리스트 구현(C)
+// 이중 연결 리스트: DNode(data, Llink, Rlink)
 // 새로운 노드(DNode: data, Llink, Rlink) 생성
 DNode* makeDNode(element num) {
 	DNode* newNode = (DNode*)malloc(sizeof(DNode));
