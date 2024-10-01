de92ae62a47bcb5c47e61f2bb2b4666bf4d8b6a2
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-01 21:09:47 +0900

    Update 04_SLinkedList(head).h

--- cpp/04_SLinkedList(head).h
+++ cpp/04_SLinkedList(head).h
@@ -27,7 +27,7 @@ public:
 	SNode*	rearNode(void) const;		// 탐색: 맨 마지막 노드(tail_)
 	void	addRear(const int& e);		// 삽입: 맨 마지막 노드(tail_)
 	void	removeFront(void);		// 삭제: 첫 번째 노드(head_)
-	void	printLinkedList(void) const;	// 리스트의 전체 원소(노드) 출력
+	void	printLinkedList(void) const;	// 출력: 리스트 전체 노드의 데이터
 private:
 	SNode*		head_;		// 첫 번째 노드
 	// SNode*	tail_;		// 맨 마지막 노드
