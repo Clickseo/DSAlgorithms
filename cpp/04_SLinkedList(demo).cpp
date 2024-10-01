adbf318d735b7f69a84559b85d4b2c1ff3259877
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-01 15:04:14 +0900

    Create 04_SLinkedList(demo).cpp

---
+++ cpp/04_SLinkedList(demo).cpp
@@ -0,0 +1,30 @@
+/*
+	단순 연결 리스트: 알고리즘 구현(cpp)
+		파일명: SLinkedList(demo).cpp
+			- main	: 데이터 삽입.삭제, 전체 출력
+*/
+#include <iostream>
+#include "SLinkedList.h"			// SLinkedList >> head, count, tail
+// #include "SLinkedList(head).h"	// SLinkedList >> head
+// #include "LinkedNode.h"			// SNode
+using namespace std;
+
+int main(void)
+{
+	int			num;
+	SLinkedList	sList = SLinkedList();	// sList.SLinkedList();
+										// head_ = nullptr;
+
+	while (true) {
+		cout << "임의의 정수 입력(종료: 0): ";
+		cin >> num;
+		if (num == 0)
+			break;
+		// 맨 마지막 노드로 삽입
+		sList.addRear(num);
+	}
+
+	// 전체 원소 출력
+	sList.printLinkedList();
+	return 0;		// sList.~SLinkedList();
+}
