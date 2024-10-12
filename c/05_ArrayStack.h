9df7bbf04b1067b7a3a0624a9c2e14eff0c61aaa
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-13 01:12:13 +0900

    Update 05_ArrayStack.h

--- c/05_ArrayStack.h
+++ c/05_ArrayStack.h
@@ -1,17 +1,17 @@
 /*
 	스택: 알고리즘 구현(C) -- 순차 자료구조
 		파일명: ArrayStack.h
-			- 스택 생성.소멸		: stackCreate, stackDestroy
-			- 스택 상태		: stackEmpty, stackFull
-			- 스택 크기		: stackSize
+			- 스택 생성.소멸	: stackCreate, stackDestroy
+			- 스택 상태			: stackEmpty, stackFull
+			- 스택 크기			: stackSize
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인(peek)	: top
 			- 전체 데이터 출력	: printStack
 */
 
 // #pragma once
-#define		StackMAXSIZE 1024
-typedef	int	element;
+#define			StackMAXSIZE 1024
+typedef	int		element;
 
 // 스택(순차 자료구조): ArrayStack
 #ifndef __ArrayStack_H__
@@ -19,19 +19,19 @@ typedef	int	element;
 
 // 구조체 설계: ArrayStack
 typedef struct _ArrayStack {
-	int		top;
+	int			top;
 	element		stack[StackMAXSIZE];
 } ArrayStack;
 
 #endif
 
 // ArrayStack: 스택 생성 및 조작 함수
-ArrayStack	*stackCreate(void);				// 빈 스택 생성
+ArrayStack	*stackCreate(void);						// 빈 스택 생성
 void		stackDestroy(ArrayStack *Stack);		// 스택 삭제
 _Bool		stackEmpty(ArrayStack *Stack);			// 빈 스택 여부(top)
 _Bool		stackFull(ArrayStack *Stack);			// 스택의 포화 상태 여부(top)
-int			stackSize(ArrayStack *Stack);		// 스택의 원소 개수(top)
-void		push(ArrayStack *Stack, element data);		// 데이터 삽입: 스택에 데이터 추가(top)
-void		pop(ArrayStack *Stack);				// 데이터 삭제: 스택에서 데이터 제거(top)
-element		top(ArrayStack *Stack);				// 데이터 확인(peek): 맨 위에 있는 데이터 확인(top)
+int			stackSize(ArrayStack *Stack);			// 스택의 원소 개수(top)
+void		push(ArrayStack *Stack, element data);	// 데이터 삽입: 스택에 데이터 추가(top)
+void		pop(ArrayStack *Stack);					// 데이터 삭제: 스택에서 데이터 제거(top)
+element		top(ArrayStack *Stack);					// 데이터 확인(peek): 맨 위에 있는 데이터 확인(top)
 void		printStack(ArrayStack *Stack);			// 데이터 출력: 스택의 전체 데이터 출력
