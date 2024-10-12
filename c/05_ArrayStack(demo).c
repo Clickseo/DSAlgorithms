16934613211b6e3b7c4263eefd20513e6f8bed54
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-13 01:13:45 +0900

    Update 05_ArrayStack(demo).c

--- c/05_ArrayStack(demo).c
+++ c/05_ArrayStack(demo).c
@@ -1,18 +1,18 @@
 /*
-	스택(순차자료구조): 알고리즘 구현(C)
+	스택(순차 자료구조): 알고리즘 구현(C)
 		파일명: ArrayStack(demo).c
 			- main	: 데이터 삽입.삭제, 전체 출력
 */
 
 #include <stdio.h>
 #include <stdbool.h>		// bool, true, false
-#include <stdlib.h>		// system, exit
-#include <conio.h>		// _getch, _getche
+#include <stdlib.h>			// system, exit
+#include <conio.h>			// _getch, _getche
 #include "ArrayStack.h"		// ArrayStack
 
 int main(void)
 {
-	int	num, choice;
+	int			num, choice;
 
 	// 빈 스택 생성: stack[StackMAXSIZE], top = -1
 	ArrayStack	*s = stackCreate();
@@ -25,33 +25,33 @@ int main(void)
 		printf("3) 데이터 전체 출력 \n");
 		printf("4) 프로그램 종료 \n\n");
 		printf("메뉴 선택: ");
-		scanf_s("%d%*c", &choice);
-		// scanf("%d", &choice);		
-		// while (getchar() != EOF);
+		scanf_s("%d", &choice);			// scanf("%d", &choice);		
+		while (getchar() != '\n');
 
 		switch (choice) {
 			case 1: 
 				while (true) {
 					printf("데이터 입력(종료: 0): ");
 					scanf_s("%d", &num);	// scanf("%d", &num);
+					while (getchar() != '\n');
 					if (num == 0)
 						break;
-					push(tStack, num);
+					push(s, num);
 				}
 				break;
 			case 2: printf("삭제 된 데이터: %3d \n", top(s));
-				pop(s);
-				break;
+					pop(s);
+					break;
 			case 3: printStack(s);
-				break;
-			case 4: printf("프로그램 종료... \n");
-				exit(0);	// return 0;
+					break;
+			case 4: printf("프로그램 종료!!!\n");
+					exit(0);	// return 0;
 			default: printf("잘못 선택 하셨습니다!!!\n");
 		}
 		// print("계속하려면 아무 키나 누르십시오...");
 		// _getch();
 		system("pause");
 	}
-	// stackDestroy(s);
+	// stackDestroy(tStack);
 	return 0;
 }
