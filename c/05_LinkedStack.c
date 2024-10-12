dbc018bb44e9e5de6871d376435501f8f28d0df2
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-13 01:17:39 +0900

    Update 05_LinkedStack.c

--- c/05_LinkedStack.c
+++ c/05_LinkedStack.c
@@ -1,18 +1,18 @@
 /*
 	스택: 알고리즘 구현(C) -- 단순연결리스트
 		파일명: LinkedStack.c
-			- 스택 생성.소멸	: stackCreate, stackDestroy
-			- 스택 상태			: stackEmpty, stackFull
-			- 스택 크기			: stackSize
+			- 스택 생성.소멸		: stackCreate, stackDestroy
+			- 스택 상태		: stackEmpty, stackFull
+			- 스택 크기		: stackSize
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인(peek)	: top
 			- 전체 데이터 출력	: printStack
 */
 
 #include <stdio.h>
-#include <stdlib.h>				// malloc, free
-#include "LinkedStack.h"		// LinkedStack, SNode
-// #include "LinkedNode.h"		// SNode
+#include <stdlib.h>		// malloc, free
+#include "LinkedStack.h"	// LinkedStack, SNode
+// #include "LinkedNode.h"	// SNode
 
 // 빈 스택 생성
 LinkedStack	*stackCreate(void) {
