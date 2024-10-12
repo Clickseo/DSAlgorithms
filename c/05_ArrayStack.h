0922f0d4aa522f27f27cea6f2216d5f77942d02a
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-12 23:17:43 +0900

    Update 05_ArrayStack.h

--- c/05_ArrayStack.h
+++ c/05_ArrayStack.h
@@ -1,9 +1,9 @@
 /*
 	스택: 알고리즘 구현(C) -- 순차 자료구조
 		파일명: ArrayStack.h
-			- 스택 생성.소멸	: stackCreate, stackDestroy
-			- 스택 상태			: stackEmpty, stackFull
-			- 스택 크기			: stackSize
+			- 스택 생성.소멸		: stackCreate, stackDestroy
+			- 스택 상태		: stackEmpty, stackFull
+			- 스택 크기		: stackSize
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인(peek)	: top
 			- 전체 데이터 출력	: printStack
@@ -19,7 +19,7 @@ typedef	int		element;
 
 // 구조체 설계: ArrayStack
 typedef struct _ArrayStack {
-	int			top;
+	int		top;
 	element		stack[StackMAXSIZE];
 } ArrayStack;
 
@@ -30,7 +30,7 @@ ArrayStack	*stackCreate(void);
 void		stackDestroy(ArrayStack *Stack);
 _Bool		stackEmpty(ArrayStack *Stack);
 _Bool		stackFull(ArrayStack *Stack);
-int			stackSize(ArrayStack *Stack);
+int		stackSize(ArrayStack *Stack);
 void		push(ArrayStack *Stack, element data);
 void		pop(ArrayStack *Stack);
 element		top(ArrayStack *Stack);
