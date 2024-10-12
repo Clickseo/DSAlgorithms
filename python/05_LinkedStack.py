5961439ac308b6f4150c2e20a5980a8913efac00
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-12 19:30:24 +0900

    Update 05_LinkedStack.py

--- python/05_LinkedStack.py
+++ python/05_LinkedStack.py
@@ -12,63 +12,14 @@
 '''
 
 # SNode class
-class SNode :
+class SNode:
     def __init__(self, data, link=None):
         self.data = data
         self.link = link
 
 # LinkedStack class
-class LinkedStack :
-    # 생성자: 빈 스택 생성
-    def __init__(self):
-        self.__top = None
-        self.__count = 0
+class LinkedStack:
 
-    # 소멸자: 스택 삭제(모든 노드 삭제)
-    def __del__(self):
-        temp = self.__top
-        while temp :
-            self.__top = temp.link
-            del temp
-            temp = self.__top
-        del self
-        
-    # push: 스택의 데이터 삽입
-    def push(self, data) -> None :
-        nSNode = SNode(data, self.__top)
-        self.__top = nSNode
-
-    # pop: 스택에서 데이터 삭제
-    def pop(self) -> None :
-        if self.__top == None :
-            return None
-        temp = self.__top
-        self.__top = temp.link
-
-    # top(peek): 스택 맨 위의 원소 확인
-    def top(self):
-        if self.__top == None :
-            return None
-        return self.__top.data
-
-    # empty: 스택의 공백 상태
-    def empty(self) -> bool :
-        return self.__top == None
-    
-    # size: 스택의 크기
-    def size(self) -> int :
-        return self.__count
-    
-    # 스택의 전체 원소 출력
-    def printStack(self):
-        temp = self.__top
-        print('\n STACK [', end = ' ')
-        while temp :
-            print(temp.data, end = ' ')
-            temp = temp.link
-        print(']\n')
-
-# from LinkedStack import LinkedStack
 if __name__ == '__main__':
     S = LinkedStack()
     while(True):
