6057837f3c24015d04ceee7792dcb05f31254332
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-13 15:59:04 +0900

    Update 05_LinkedStack.h

--- c/05_LinkedStack.h
+++ c/05_LinkedStack.h
@@ -1,11 +1,11 @@
 /*
 	스택: 알고리즘 구현(C) -- 단순 연결 리스트
 		파일명: LinkedStack.h
-			- 스택 생성.소멸		: stackCreate, stackDestroy
-			- 빈 스택 여부 판단		: stackEmpty
-			- 스택 크기				: stackSize
-			- 데이터 삽입.삭제		: push, pop
-			- 데이터 확인(peek)		: top
+			- 스택 생성.소멸	: stackCreate, stackDestroy
+			- 빈 스택 여부 판단	: stackEmpty
+			- 스택 크기		: stackSize
+			- 데이터 삽입.삭제	: push, pop
+			- 데이터 확인(peek)	: top
 			- 스택의 전체 원소 출력	: printStack
 */
 
@@ -19,17 +19,17 @@
 // 구조체 설계: LinkedStack
 typedef struct _LinkedStack {
 	SNode		*top;
-	int			count;
+	int		count;
 } LinkedStack;
 
 #endif
 
 // LinkedStack: 스택 생성 및 조작 함수
-LinkedStack	*stackCreate(void);							// 빈 스택 생성
-void		stackDestroy(LinkedStack *Stack);			// 스택 삭제
-_Bool		stackEmpty(LinkedStack *Stack);				// 빈 스택 여부(top)
-int			stackSize(LinkedStack *Stack);				// 스택의 원소 개수(top)
+LinkedStack	*stackCreate(void);				// 빈 스택 생성
+void		stackDestroy(LinkedStack *Stack);		// 스택 삭제
+_Bool		stackEmpty(LinkedStack *Stack);			// 빈 스택 여부(top)
+int		stackSize(LinkedStack *Stack);			// 스택의 원소 개수(top)
 void		push(LinkedStack *Stack, element data);		// 데이터 삽입(top)
-void		pop(LinkedStack *Stack);					// 데이터 삭제(top)
-element		top(LinkedStack *Stack);					// 데이터 확인(peek)(top)
-void		printStack(LinkedStack* Stack);				// 스택의 전체 데이터 출력
+void		pop(LinkedStack *Stack);			// 데이터 삭제(top)
+element		top(LinkedStack *Stack);			// 데이터 확인(peek)(top)
+void		printStack(LinkedStack* Stack);			// 스택의 전체 데이터 출력
