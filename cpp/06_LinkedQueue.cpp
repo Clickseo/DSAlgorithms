8965a4199f72be5fb4063428eaebdddb9fbb13ab
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-10-30 10:46:44 +0900

    Update 06_LinkedQueue.cpp

--- cpp/06_LinkedQueue.cpp
+++ cpp/06_LinkedQueue.cpp
@@ -1,9 +1,9 @@
 /*
 	큐: 알고리즘 구현(CPP) -- 단순연결리스트
 		파일명: LinkedQueue.cpp
-			- 스택 생성.소멸	: LinkedQueue, ~LinkedQueue
-			- 스택 상태			: empty
-			- 스택 크기			: size
+			- 스택 생성.소멸		: LinkedQueue, ~LinkedQueue
+			- 스택 상태		: empty
+			- 스택 크기		: size
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인(peek)	: front, back
 			- 전체 데이터 출력	: printQueue
@@ -22,7 +22,7 @@ public:
 	SNode(const T &data);
 	T	getData(void) const;
 private:
-	T			data_;
+	T		data_;
 	SNode<T>	*link_;
 	template <typename T> friend class LinkedQueue;
 };
@@ -41,17 +41,17 @@ T   SNode<T>::getData(void) const {
 template <typename T>
 class LinkedQueue {
 public:
-	LinkedQueue();						// 빈 큐 생성
-	~LinkedQueue();						// 큐 삭제: 모든 노드 삭제
-	bool	empty(void) const;			// 빈 큐 여부
-	int		size(void) const;			// 큐의 원소 개수
+	LinkedQueue();				// 빈 큐 생성
+	~LinkedQueue();				// 큐 삭제: 모든 노드 삭제
+	bool	empty(void) const;		// 빈 큐 여부
+	int	size(void) const;		// 큐의 원소 개수
 	void	push(const T &data);		// 데이터 삽입
-	void	pop(void);					// 데이터 삭제
-	T		front(void) const;			// 큐의 첫번째 데이터 반환
-	T		back(void) const;			// 큐의 맨 마지막 데이터 반환
+	void	pop(void);			// 데이터 삭제
+	T	front(void) const;		// 큐의 첫번째 데이터 반환
+	T	back(void) const;		// 큐의 맨 마지막 데이터 반환
 	void	printQueue(void) const;		// 큐의 전체 데이터 출력
 private:
 	SNode<T>	*front_;
 	SNode<T>	*rear_;
-	int			count_;
+	int		count_;
 };
