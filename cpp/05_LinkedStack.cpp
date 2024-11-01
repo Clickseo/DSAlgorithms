26ac9ff5145c66c144bca34209bdb80601c37f5e
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-02 00:31:15 +0900

    Update 05_LinkedStack.cpp

--- cpp/05_LinkedStack.cpp
+++ cpp/05_LinkedStack.cpp
@@ -1,16 +1,16 @@
 /*
 	스택: 알고리즘 구현(C++) -- 단순연결리스트
 		파일명: LinkedStack.cpp
-			- 스택 생성.소멸	: LinkedStack, ~LinkedStack
-			- 스택 상태			: empty
-			- 스택 크기			: size
+			- 스택 생성.소멸		: LinkedStack, ~LinkedStack
+			- 스택 상태		: empty
+			- 스택 크기		: size
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인		: top
 			- 전체 데이터 출력	: printStack
 */
 
 #include <iostream>
-#include "LinkedNode(template).cpp"
+#include "LinkedNode(template).cpp"	// SNode<T>
 using namespace std;
 
 // #pragma once
@@ -22,18 +22,17 @@ template <typename T>
 class LinkedStack {
 public:
 	LinkedStack();				// 빈 스택 생성
-	~LinkedStack();				// 스택 삭제: 모든 노드 삭제	
+	~LinkedStack();				// 스택 삭제
 	bool	empty(void) const;		// 빈 스택 여부
-	int	size(void) const;		// 스택의 원소 개수
+	int	size(void) const;		// 원소 개수
 	void	push(const T &data);		// 데이터 삽입
 	void	pop(void);			// 데이터 삭제
 	T	top(void) const;		// 맨 위의 데이터 반환
-	void	printStack(void) const;		// 스택의 전체 데이터 출력
+	void	printStack(void) const;		// 전체 데이터 출력
 private:
-	SNode<T>	*top_;			// 맨 위의 노드
-	int		count_;			// 스택의 원소 개수
+	SNode<T>	*top_;
+	int		count_;
 };
-
 
 // 빈 스택 생성
 template <typename T>
@@ -57,7 +56,7 @@ bool	LinkedStack<T>::empty(void) const {
 	return top_ == nullptr;
 }
 
-// 스택의 원소 개수
+// 원소 개수
 template <typename T>
 int	LinkedStack<T>::size(void) const {
 	return count_;
@@ -94,7 +93,7 @@ T	LinkedStack<T>::top(void) const {
 	return  top_->data_;
 }
 
-// 스택의 전체 데이터 출력
+// 전체 데이터 출력
 template <typename T>
 void	LinkedStack<T>::printStack(void) const {
 	if (empty()) {
