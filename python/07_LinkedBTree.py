8090cedd5635cb3022e5eb2b448aa30d0a2b74c7
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-05 21:27:18 +0900

    Update 07_LinkedBTree.py

--- python/07_LinkedBTree.py
+++ python/07_LinkedBTree.py
@@ -2,16 +2,12 @@
 	이진 트리: 알고리즘 구현(Python) -- 이중연결리스트
         파일명: LinkedBTree.py
         	- __main__: 데이터 삽입.삭제, 전체 원소 출력
-			- 클래스: DNode
-			- 클래스: LinkedBTree
-				- 이진 트리 생성: makeLinkedBTree
-				- 깊이 우선 순회: Preorder, Inorder, Postorder
-				- 너비 우선 순회: Levelorder
+		- 클래스: DNode
+		- 클래스: LinkedBTree
+			- 이진 트리 생성: makeLinkedBTree
+			- 깊이 우선 순회: Preorder, Inorder, Postorder
+			- 너비 우선 순회: Levelorder
 '''
-
-# 연산자 여부 판단
-def isOperator(op) -> bool:
-    return op == '+' or op == '-' or op == '*' or op == '/'
 
 # 클래스 설계: LinkedBTree
 class LinkedBTree:
@@ -23,9 +19,6 @@ class LinkedBTree:
 
     def __init__(self):
         self.__root = None
-
-    # 이진 트리(수식 트리) 생성: 스택 구조 활용
-    def makeLinkedBTree(self, postfix) -> DNode:  
         
     # 깊이 우선 순회: 전위.중위.후위 순회
     def Preorder(self) -> None:
@@ -34,13 +27,17 @@ class LinkedBTree:
 
     # 너비 우선 순회
     def Levelorder(self) -> None
-    def __del__(self):	
+    def __del__(self):
+
+    # 이진 트리(수식 트리) 생성: 스택 구조 활용
+    def makeLinkedBTree(self, postfix) -> None:
               
 if __name__ == '__main__':     
     postfix = input('트리를 구성할 후위 수식: ')
+
+    # 이진 트리(수식 트리) 생성
     BTree = LinkedBTree()
     BTree.makeLinkedBTree(postfix)
-    # root = bTree.makeLinkedBTree(postfix)
 
     # 깊이 우선 순회: 전위.중위.후위 순회
     print('Preorder  : ', end=' ');     BTree.Preorder();    print('')
