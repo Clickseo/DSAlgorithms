9670198512c707eb71a3d6215b461fd34b21c3d2
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-05 21:29:06 +0900

    Update 07_LinkedBTree.py

--- python/07_LinkedBTree.py
+++ python/07_LinkedBTree.py
@@ -19,19 +19,48 @@ class LinkedBTree:
 
     def __init__(self):
         self.__root = None
-        
-    # 깊이 우선 순회: 전위.중위.후위 순회
+
+    # 깊이 우선 순회: 전위 순회
     def Preorder(self) -> None:
+        def _Preorder(tNode):
+            if tNode:
+                print(tNode.data, end=' ')
+                _Preorder(tNode.Llink)
+                _Preorder(tNode.Rlink)        
+        _Preorder(self.__root)
+
+    # 깊이 우선 순회: 중위 순회
     def Inorder(self) -> None:
+        def _Inorder(tNode):
+            if tNode:                
+                _Inorder(tNode.Llink)
+                print(tNode.data, end=' ')
+                _Inorder(tNode.Rlink)
+        _Inorder(self.__root)
+
+    # 깊이 우선 순회: 후위 순회
     def Postorder(self) -> None:
+        def _Postorder(tNode):
+            if tNode: 
+                _Postorder(tNode.Llink)
+                _Postorder(tNode.Rlink)
+                print(tNode.data, end=' ')
+        _Postorder(self.__root)
 
     # 너비 우선 순회
-    def Levelorder(self) -> None
+    def Levelorder(self) -> None:
+        pass
+	    
     def __del__(self):
+        pass
 
     # 이진 트리(수식 트리) 생성: 스택 구조 활용
     def makeLinkedBTree(self, postfix) -> None:
-              
+        # 연산자 여부 판단
+        def isOperator(op) -> bool:
+            return op == '+' or op == '-' or op == '*' or op == '/'
+        pass
+       
 if __name__ == '__main__':     
     postfix = input('트리를 구성할 후위 수식: ')
 
