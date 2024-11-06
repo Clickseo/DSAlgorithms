424873ba5d38d6ff7a750e84ce66ed6d0065367e
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-06 19:25:55 +0900

    Update 05_LinkedStack(demo).cpp

--- cpp/05_LinkedStack(demo).cpp
+++ cpp/05_LinkedStack(demo).cpp
@@ -5,7 +5,7 @@
 */
 
 #include <iostream>
-#include "LinkedStack.cpp"	// LinkedStack, SNode<T>
+#include "LinkedStack.cpp"	// LinkedStack<T>, SNode<T>
 // #include <conio.h>		// _getch, _getche
 using namespace std;
 
