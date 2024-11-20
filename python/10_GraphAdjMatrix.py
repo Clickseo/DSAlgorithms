1f4454b6636232c5169aceb3d904ecb97553212c
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-20 13:14:07 +0900

    Update 10_GraphAdjMatrix.py

--- python/10_GraphAdjMatrix.py
+++ python/10_GraphAdjMatrix.py
@@ -1,33 +1,32 @@
 '''
 	그래프 표현(인접 행렬): 알고리즘 구현
 		파일명: GraphAdjMatrix.py
-			- main	: 그래프 생성 및 간선 추가
-			- 클래스	: GraphType
-				그래프 생성 : __init__
-				그래프 간선 추가 : insertEdge
-				그래프 전체 출력 : printAdjMatrix
+			- main: 그래프 생성 및 간선 추가
+			클래스: GraphType
+				- 그래프 생성: __init__
+				- 간선 추가	 : insertEdge
+				- 전체 출력	 : printAdjMatrix
 '''
 
-# GraphType class
-class GraphType :
-    # 생성자: 그래프 생성
-    def __init__(self, vertex: int) : 
-        self.__vertex = vertex    # 정점의 개수
-        self.__adjMatrix = []     # 인접 행렬
+# 클래스 설계: GraphType
+class GraphType:
+    def __init__(self, vertex:int): 
+        self.__vertex = vertex      # 정점의 개수
+        self.__adjMatrix = []       # 인접 행렬: 2차원 배열
         for i in range(self.__vertex) : 
             s = []
             for j in range(self.__vertex) :
-                s.append(0);
-            self.__adjMatrix.append(s)
+                s.append(0)
+            self.__adjMatrix.append(s)         
 
-    # 그래프 간선 추가
-    def insertEdge(self, row: int, col: int, weight: int) :
+    # insertEdge: 간선 추가
+    def insertEdge(self, row:int, col:int, weight:int) :
         if row >= self.__vertex or col >= self.__vertex :
-            print('그래프에 없는 정점입니다!!!')
+            # print('그래프에 없는 정점입니다!!!')
             return
         self.__adjMatrix[row][col] = weight
         
-    # 그래프 전체 출력
+    # printAdjMatrix: 그래프 전체 출력
     def printAdjMatrix(self) :
         for i in range(self.__vertex) :
             print('\t', end='')
@@ -36,7 +35,7 @@ class GraphType :
             print('')
 
 if __name__ == '__main__' :
-    # G2 : 무향 그래프
+    # G2: 무향 그래프
     G2 = GraphType(3)
 
     # 정점: A(0)
@@ -57,13 +56,13 @@ if __name__ == '__main__' :
     # G4 : 유향 그래프
     G4 = GraphType(3)
 
-    # 정점(A)
+    # 정점: A(0)
     G4.insertEdge(0, 1, 0)	# A(0) - B(1)
     G4.insertEdge(0, 2, 0)	# A(0) - C(2)
 
-    # 정점(B)
+    # 정점: B(0)
     G4.insertEdge(1, 0, 0)	# B(1) - A(0)
     G4.insertEdge(1, 2, 0)	# B(1) - C(2)
 
-    print(f'\n##### 그래프(G4): 인접 행렬 #####\n');
+    print('\n##### 그래프(G4): 인접 행렬 #####\n')
     G4.printAdjMatrix()
