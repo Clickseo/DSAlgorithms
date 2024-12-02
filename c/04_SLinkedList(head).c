4e4f1e2d239bc3800e6b2bcd2a327ec28a312a77
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-12-02 23:07:39 +0900

    Update 04_SLinkedList(head).c

--- c/04_SLinkedList(head).c
+++ c/04_SLinkedList(head).c
@@ -14,8 +14,8 @@
 // #include "LinkedNode.h"	// SNode, makeSNode
 
 // 빈 리스트 생성
-SLinkedList* sListCreate(void) {
-	SLinkedList* sList = (SLinkedList*)malloc(sizeof(SLinkedList));
+SLinkedList *sListCreate(void) {
+	SLinkedList *sList = (SLinkedList*)malloc(sizeof(SLinkedList));
 	if (sList == NULL) {
 		printf("메모리 할당 실패!!! \n");
 		exit(1);
@@ -25,7 +25,7 @@ SLinkedList* sListCreate(void) {
 }
 
 // 리스트 삭제: 리스트의 전체 노드 삭제
-SLinkedList* sListDestroy(SLinkedList* sList) {
+SLinkedList *sListDestroy(SLinkedList *sList) {
 	// while (!sListEmpty(sList))
 	//	sListRemoveFront(sList);
 	// free(sList);
@@ -41,17 +41,17 @@ SLinkedList* sListDestroy(SLinkedList* sList) {
 }
 
 // 빈 리스트 여부 판단
-_Bool	sListEmpty(SLinkedList* sList) {
+_Bool	sListEmpty(SLinkedList *sList) {
 	return sList->head == NULL;
 }
 
 // 탐색: 노드의 총 개수
-int	countSNode(SLinkedList* sList) {
+int	countSNode(SLinkedList *sList) {
 	if (sListEmpty(sList))
 		return 0;
 	
 	int	count = 0;
-	SNode* rNode = sList->head;
+	SNode *rNode = sList->head;
 	while (rNode) {
 		count++;
 		rNode = rNode->link;
@@ -60,51 +60,51 @@ int	countSNode(SLinkedList* sList) {
 }
 
 // 탐색: 첫 번째 노드
-SNode* frontSNode(SLinkedList* sList) {
+SNode *frontSNode(SLinkedList *sList) {
 	return sList->head;
 }
 
 // 탐색: 맨 마지막 노드
-SNode* rearSNode(SLinkedList* sList) {
+SNode *rearSNode(SLinkedList *sList) {
 	if (sListEmpty(sList))
 		return NULL;
 	
-	SNode* rNode = sList->head;
+	SNode *rNode = sList->head;
 	while (rNode->link)
 		rNode = rNode->link;
 	return rNode;
 }
 
 // 노드 삽입: 맨 마지막 노드로...
-void sListAddRear(SLinkedList* sList, SNode* newNode) {
+void sListAddRear(SLinkedList *sList, SNode *newNode) {
 	if (sListEmpty(sList)) {
 		sList->head = newNode;
 	}
 	else {
-		SNode* rNode = rearSNode(sList);
+		SNode *rNode = rearSNode(sList);
 		rNode->link = newNode;
 	}
 }
 
 // 노드 삭제: 첫 번째 노드를...
-void	sListRemoveFront(SLinkedList* sList) {
+void	sListRemoveFront(SLinkedList *sList) {
 	if (sListEmpty(sList))
 		return;
 	
-	SNode* old = sList->head;
+	SNode *old = sList->head;
 	sList->head = old->link;
 	free(old);
 }
 
 // 리스트의 전체 노드 출력
-void	printSLinkedList(SLinkedList* sList) {
+void	printSLinkedList(SLinkedList *sList) {
 	if (sListEmpty(sList)) {
 		printf("입력된 데이터가 없습니다... \n");
 		return;
 	}
 	
 	printf("\n ### 입력된 데이터 ### \n\n");
-	SNode* tNode = sList->head;
+	SNode *tNode = sList->head;
 	while (tNode) {
 		printf("%3d ->>", tNode->data);
 		tNode = tNode->link;
