a17b4e0eda4eb1b622ae4b3121e627bcf4f9b2b3
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-12-02 23:06:10 +0900

    Update 04_SLinkedList(head).h

--- c/04_SLinkedList(head).h
+++ c/04_SLinkedList(head).h
@@ -18,20 +18,20 @@
 #define __SLinkedList_H__
 
 typedef struct _SLinkedList {
-	SNode*		head;	// 첫 번째 노드
-	// SNode*	tail;	// 맨 마지막 노드
+	SNode		*head;	// 첫 번째 노드
+	// SNode	*tail;	// 맨 마지막 노드
 	// int		count;	// 노드의 총 개수
 }SLinkedList;
 
 #endif
 
 // 단순 연결 리스트 구현(C): 리스트 생성 및 조작 함수
-SLinkedList*	sListCreate(void);				// 빈 리스트 생성
-SLinkedList*	sListDestroy(SLinkedList* sList);		// 리스트 삭제: 리스트의 전체 노드 삭제	
-void	sListAddRear(SLinkedList* sList, SNode* newNode);	// 삽입: 리스트의 맨 마지막 노드로...
-void	sListRemoveFront(SLinkedList* sList);			// 삭제: 리스트에서 첫 번째 노드를...
-SNode*	frontSNode(SLinkedList* sList);				// 탐색: 리스트의 첫 번째 노드(head)
-SNode*	rearSNode(SLinkedList* sList);				// 탐색: 리스트의 맨 마지막 노드
-_Bool	sListEmpty(SLinkedList* sList);				// 빈 리스트 여부 판단
-int	countSNode(SLinkedList* sList);				// 탐색: 노드의 총 개수(count)
-void	printSLinkedList(SLinkedList* sList);			// 리스트의 전체 노드 출력
+SLinkedList	*sListCreate(void);				// 빈 리스트 생성
+SLinkedList	*sListDestroy(SLinkedList *sList);		// 리스트 삭제: 리스트의 전체 노드 삭제	
+void	sListAddRear(SLinkedList *sList, SNode *newNode);	// 삽입: 리스트의 맨 마지막 노드로...
+void	sListRemoveFront(SLinkedList *sList);			// 삭제: 리스트에서 첫 번째 노드를...
+SNode	*frontSNode(SLinkedList *sList);			// 탐색: 리스트의 첫 번째 노드(head)
+SNode	*rearSNode(SLinkedList *sList);				// 탐색: 리스트의 맨 마지막 노드
+_Bool	sListEmpty(SLinkedList *sList);				// 빈 리스트 여부 판단
+int	countSNode(SLinkedList *sList);				// 탐색: 노드의 총 개수(count)
+void	printSLinkedList(SLinkedList *sList);			// 리스트의 전체 노드 출력
