d6f81a8e498267b0df34166ddfe2558ca7af25b5
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-10 21:33:36 +0900

    Update 04_SLinkedList(head).h

--- cpp/04_SLinkedList(head).h
+++ cpp/04_SLinkedList(head).h
@@ -2,12 +2,12 @@
 	단순 연결 리스트: 알고리즘 구현(cpp)
 		파일명: SLinkedList(head).h
 			- 클래스: SLinkedList
-				- 스택 생성.소멸	: SLinkedList, ~SLinkedList
-				- 데이터 삽입.삭제	: addRear, removeFront
-				- 데이터 확인(탐색)	: frontNode, rearNode
+				- 스택 생성.소멸			: SLinkedList, ~SLinkedList
+				- 데이터 삽입.삭제		: addRear, removeFront
+				- 데이터 확인(탐색)		: frontNode, rearNode
 				- 빈 리스트 여부 판단	: isEmpty
-				- 리스트의 총 개수	: countNode
-				- 리스트의 전체 출력	: printLinkedList
+				- 리스트의 총 개수		: countNode
+				- 리스트의 전체 출력		: printLinkedList
 */
 
 // #pragma once
@@ -19,18 +19,18 @@
 // SLinkedList class
 class SLinkedList {
 public:
-	SLinkedList(void);			// 생성자
-	~SLinkedList(void);			// 소멸자: 전체 노드 삭제
-	bool	isEmpty(void) const;		// 빈 리스트 여부 판단
-	int	countNode(void) const;		// 탐색: 노드의 총 개수(count_)
-	SNode*	frontNode(void) const;		// 탐색: 첫 번째 노드(head_)
-	SNode*	rearNode(void) const;		// 탐색: 맨 마지막 노드(tail_)
-	void	addRear(const int& e);		// 삽입: 맨 마지막 노드(tail_)
-	void	removeFront(void);		// 삭제: 첫 번째 노드(head_)
+	SLinkedList(void);						// 생성자
+	~SLinkedList(void);						// 소멸자: 전체 노드 삭제
+	bool	isEmpty(void) const;			// 빈 리스트 여부 판단
+	int	countNode(void) const;				// 탐색: 노드의 총 개수(count_)
+	SNode	*frontNode(void) const;			// 탐색: 첫 번째 노드(head_)
+	SNode	*rearNode(void) const;			// 탐색: 맨 마지막 노드(tail_)
+	void	addRear(const int& e);			// 삽입: 맨 마지막 노드(tail_)
+	void	removeFront(void);				// 삭제: 첫 번째 노드(head_)
 	void	printLinkedList(void) const;	// 출력: 리스트 전체 노드의 데이터
 private:
-	SNode*		head_;		// 첫 번째 노드
-	// SNode*	tail_;		// 맨 마지막 노드
+	SNode		*head_;		// 첫 번째 노드
+	// SNode	*tail_;		// 맨 마지막 노드
 	// int	  	count_;		// 노드의 총 개수
 };
 
