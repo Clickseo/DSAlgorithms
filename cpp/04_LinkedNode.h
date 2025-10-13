96c807bb5798ed55c3f8945c3aeb4a8812befa12
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-13 19:03:23 +0900

    Update 04_LinkedNode.h

--- cpp/04_LinkedNode.h
+++ cpp/04_LinkedNode.h
@@ -1,37 +1,41 @@
 /*
-	단순.이중 연결 리스트 : 알고리즘 구현(cpp)
+	단순.이중 연결 리스트 : 알고리즘 구현(C++)
 		파일명: LinkedNode.h
-			- 클래스: SNode(data, link) -- 단순 연결 리스트(노드)
-			- 클래스: DNode(data, Llink, Rlink) -- 이중 연결 리스트(노드)
+			- 클래스: SNode(data, link)			/ 단순 연결 리스트(노드)	: 
+			- 클래스: DNode(data, Llink, Rlink)	/ 이중 연결 리스트(노드)	: 
 */
 
-// 단순 연결 리스트: SNode(data, link)
 // #pragma once
+
 #ifndef __SNode_H__
 #define __SNode_H__
 
-// SNode class
+// 클래스 설계: SNode class
+// 노드: SNode(data, link)
 class SNode {
 public:
 	SNode(const int &data);
+	int	getData(void) const;
 private:
-	int	data_;
+	int		data_;
 	SNode	*link_;
 	friend class SLinkedList;
 };
 
 #endif
 
-// 이중 연결 리스트: DNode(data, Llink, Rlink)
+
 #ifndef __DNode_H__
 #define __DNode_H__
 
-// DNode class
+// 클래스 설계: DNode class
+// 노드: DNode(data, Llink, Rlink)
 class DNode {
 public:
 	DNode(const int &data);
+	int	getData(void) const;
 private:
-	int	data_;
+	int		data_;
 	DNode	*Llink_;
 	DNode	*Rlink_;
 	friend class DLinkedList;
