02fed99551c4f974b358cbdc214d4c3290d13417
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-13 19:04:07 +0900

    Update 04_LinkedNode.cpp

--- cpp/04_LinkedNode.cpp
+++ cpp/04_LinkedNode.cpp
@@ -1,20 +1,29 @@
 /*
-	단순.이중 연결 리스트 : 알고리즘 구현(cpp)
-		파일명: LinkedNode.h
-			- 클래스: SNode(data, link) -- 단순 연결 리스트(노드)
-			- 클래스: DNode(data, Llink, Rlink) -- 이중 연결 리스트(노드)
+	단순.이중 연결 리스트 : 알고리즘 구현(C++)
+		파일명: LinkedNode.cpp
+			- 클래스: SNode(data, link)			/ 단순 연결 리스트(노드)
+			- 클래스: DNode(data, Llink, Rlink) / 이중 연결 리스트(노드)
 */
 
 #include <iostream>
 #include "LinkedNode.h"		// SNode, DNode
 using namespace std;
 
-// 단순 연결 리스트: SNode(data, link)
-// SNode 생성자: 새로운 노드 생성
-SNode::SNode(const int& data):
+// 클래스 설계: SNode(data, link)
+SNode::SNode(const int &data):
 	data_(data), link_(nullptr) {}
 
-// 이중 연결 리스트: DNode(data, Llink, Rlink)
-// DNode 생성자: 새로운 노드 생성
-DNode::DNode(const int& data):
+// 노드의 데이터 반환
+int	SNode::getData(void) const {
+	return data_;
+}
+
+
+// 클래스 설계: DNode(data, Llink, Rlink)
+DNode::DNode(const int &data):
 	data_(data), Llink_(nullptr), Rlink_(nullptr) {}
+
+// 노드의 데이터 반환
+int	DNode::getData(void) const {
+	return data_;
+}
