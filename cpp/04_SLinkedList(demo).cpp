a405953986f4ac1769e7f78c0ef2ac90b0e43ed9
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-13 19:06:38 +0900

    Update 04_SLinkedList(demo).cpp

--- cpp/04_SLinkedList(demo).cpp
+++ cpp/04_SLinkedList(demo).cpp
@@ -1,19 +1,19 @@
 /*
-	단순 연결 리스트: 알고리즘 구현(C++)
-		파일명: SLinkedList(demo).cpp
+	이중 연결 리스트: 알고리즘 구현(C++)
+		파일명: DLinkedList(demo).cpp
 			- main	: 데이터 삽입.삭제, 전체 출력
 */
+
 #include <iostream>
-// #include "SLinkedList(head).h"	// SLinkedList >> head
-#include "SLinkedList(tail).h"		// SLinkedList >> head, count, tail
-// #include "LinkedNode.h"			// SNode
+// #include "DLinkedList(head).h"	// DLinkedList >> head
+#include "DLinkedList(tail).h"		// DLinkedList >> head, count, tail
+// #include "LinkedNode.h"			// DNode
 using namespace std;
 
 int main(void)
 {
 	int			num;	
-	SLinkedList	sList = SLinkedList();		// head_ = nullptr;
-
+	DLinkedList	dList = DLinkedList();			// head_ = nullptr;
 	while (true) {
 		cout << "임의의 정수 입력(종료: 0): ";
 		cin >> num;
@@ -21,26 +21,27 @@ int main(void)
 			break;
 
 		// 맨 마지막 노드로 삽입
-		sList.addRear(num);
+		dList.addRear(num);
 	}
 
 	// 리스트 전체 노드의 데이터 출력
-	sList.printLinkedList();
+	dList.printLinkedList();			// 순방향 출력
+	dList.printRevLinkedList();			// 역방향 출력
 
 	// 노드의 총 개수
-	cout << "노드의 총 개수: " << sList.countNode() << endl;
+	cout << "노드의 총 개수: " << dList.countNode() << endl;
 
 	// 빈 리스트 여부 판단
-	if (sList.isEmpty()) {
+	if (dList.isEmpty()) {
 		cout << "입력된 데이터가 없습니다!!!\n" << endl;
 	}
 	else {
 		// 첫 번째 노드와 마지막 노드의 데이터
-		cout << "첫 번째 노드의 데이터: " << sList.frontNode()->getData() << endl;
-		cout << "마지막 노드의 데이터 : " << sList.rearNode()->getData() << endl;
+		cout << "첫 번째 노드의 데이터: " << dList.frontNode()->getData() << endl;
+		cout << "마지막 노드의 데이터 : " << dList.rearNode()->getData() << endl;
 	}
 	cout << endl;
 
-	// sList.~SLinkedList();
+	// dList.~DLinkedList();
 	return 0;
 }
