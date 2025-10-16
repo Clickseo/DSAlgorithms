f37658f80c69851256cf15a64fc1a6c36b255c72
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-16 11:31:20 +0900

    Update 04_DLinkedList(demo).cpp

--- cpp/04_DLinkedList(demo).cpp
+++ cpp/04_DLinkedList(demo).cpp
@@ -1,32 +1,41 @@
-/*
-	이중 연결 리스트: 알고리즘 구현(CPP)
-		파일명: DLinkedList(demo).cpp
-			- main	: 데이터 삽입.삭제, 전체 출력
-*/
-
 #include <iostream>
-#include "DLinkedList(head).h"		// DLinkedList >> head
-// #include "DLinkedList.h"			// DLinkedList >> head, count, tail
-// #include "LinkedNode.h"			// DNode
+#include "DLinkedList(head).h"	// DLinkedList >> head
+// #include "DLinkedList.h"		// DLinkedList >> head, count, tail
+// #include "LinkedNode.h"		// DNode
 using namespace std;
 
 int main(void)
 {
-	int		num;
-	DLinkedList	dList = DLinkedList();	// head_ = nullptr;
+    int		num;
+    DLinkedList	dList = DLinkedList();	// head_ = nullptr;
+    while (true) {
+	  cout << "임의의 정수 입력(종료: 0): ";
+	  cin >> num;
+	  if (num == 0)
+		break;
 
-	while (true) {
-		cout << "임의의 정수 입력(종료: 0): ";
-		cin >> num;
-		if (num == 0)
-			break;
-		// 맨 마지막 노드로 삽입
-		dList.addRear(num);
-	}
+	  // 맨 마지막 노드로 삽입
+	  dList.addRear(num);
+    }
 
-	// 전체 원소 출력
-	dList.printLinkedList();	// 순방향 출력
-	dList.printRevLinkedList();	// 역방향 출력
-	
-	return 0;	// dList.~DLinkedList();
+    // 리스트 전체 노드의 데이터 출력
+    dList.printLinkedList();			// 순방향 출력
+    dList.printRevLinkedList();		// 역방향 출력
+
+    // 노드의 총 개수
+    cout << "노드의 총 개수: " << dList.countNode() << endl;
+
+    // 빈 리스트 여부 판단
+    if (dList.isEmpty()) {
+	  cout << "입력된 데이터가 없습니다!!!\n" << endl;
+    }
+    else {
+	  // 첫 번째 노드와 마지막 노드의 데이터
+	  cout << "첫 번째 노드의 데이터: " << dList.frontNode()->getData() << endl;
+	  cout << "마지막 노드의 데이터 : " << dList.rearNode()->getData() << endl;
+    }
+    cout << endl;
+
+    // dList.~DLinkedList();
+    return 0;
 }
