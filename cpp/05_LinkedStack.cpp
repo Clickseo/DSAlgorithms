13941b96499d78a9c6e9d8815dfe9c7e81011516
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-10-30 19:34:48 +0900

    Update 05_LinkedStack.cpp

--- cpp/05_LinkedStack.cpp
+++ cpp/05_LinkedStack.cpp
@@ -2,8 +2,8 @@
 	스택: 알고리즘 구현(C++) -- 단순연결리스트
 		파일명: LinkedStack.cpp
 			- 스택 생성.소멸	: LinkedStack, ~LinkedStack
-			- 스택 상태		: empty
-			- 스택 크기		: size
+			- 스택 상태			: empty
+			- 스택 크기			: size
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인		: top
 			- 전체 데이터 출력	: printStack
@@ -21,17 +21,17 @@ using namespace std;
 template <typename T>
 class LinkedStack {
 public:
-	LinkedStack();				// 빈 스택 생성
-	~LinkedStack();				// 스택 삭제
-	bool	empty(void) const;		// 빈 스택 여부
-	int	size(void) const;		// 원소 개수
+	LinkedStack();						// 빈 스택 생성
+	~LinkedStack();						// 스택 삭제
+	bool	empty(void) const;			// 빈 스택 여부
+	int		size(void) const;			// 원소 개수
 	void	push(const T &data);		// 데이터 삽입
-	void	pop(void);			// 데이터 삭제
-	T	top(void) const;		// 맨 위의 데이터 반환
+	void	pop(void);					// 데이터 삭제
+	T		top(void) const;			// 맨 위의 데이터 반환
 	void	printStack(void) const;		// 전체 데이터 출력
 private:
 	SNode<T>	*top_;
-	int		count_;
+	int			count_;
 };
 
 #endif
