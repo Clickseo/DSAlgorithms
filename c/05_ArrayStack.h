fb6032a3a886e8610b965b8a091527368b7b8ee3
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-11-01 19:32:26 +0900

    Update 05_ArrayStack.h

--- c/05_ArrayStack.h
+++ c/05_ArrayStack.h
@@ -19,19 +19,19 @@ typedef	int		element;
 
 // 구조체 설계: ArrayStack
 typedef struct _ArrayStack {
-	int		top;
+	int			top;
 	element		stack[StackMAXSIZE];
 } ArrayStack;
 
 #endif
 
 // ArrayStack: 스택 생성 및 조작 함수
-ArrayStack	*stackCreate(void);				// 빈 스택 생성
-void		stackDestroy(ArrayStack *Stack);		// 스택 삭제: 모든 노드 삭제
-_Bool		stackEmpty(ArrayStack *Stack);			// 빈 스택 여부 확인
-_Bool		stackFull(ArrayStack *Stack);			// 스택의 포화 상태 여부 확인
-int		stackSize(ArrayStack *Stack);			// 스택의 원소 개수
+ArrayStack	*stackCreate(void);							// 빈 스택 생성
+void		stackDestroy(ArrayStack *Stack);			// 스택 삭제: 모든 노드 삭제
+_Bool		stackEmpty(ArrayStack *Stack);				// 빈 스택 여부 확인
+_Bool		stackFull(ArrayStack *Stack);				// 스택의 포화 상태 여부 확인
+int			stackSize(ArrayStack *Stack);				// 스택의 원소 개수
 void		push(ArrayStack *Stack, element data);		// 데이터 삽입
-void		pop(ArrayStack *Stack);				// 데이터 삭제
-element		top(ArrayStack *Stack);				// 스택에서 맨 위의 데이터 확인(peek)
-void		printStack(ArrayStack *Stack);			// 스택의 전체 데이터 출력
+void		pop(ArrayStack *Stack);						// 데이터 삭제
+element		top(ArrayStack *Stack);						// 스택에서 맨 위의 데이터 확인(peek)
+void		printStack(ArrayStack *Stack);				// 스택의 전체 데이터 출력
