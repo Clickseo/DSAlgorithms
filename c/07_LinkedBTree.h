6e336be8b3a94d1cb91ca034afdf2d007d25579e
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-11-01 19:48:00 +0900

    Refactor function signatures in LinkedBTree.h

--- c/07_LinkedBTree.h
+++ c/07_LinkedBTree.h
@@ -7,14 +7,14 @@
 #include "LinkedNode.h"		// DNode, makeDNode
 
 // 이진 트리 생성
-DNode*	makeLinkedBTree(char* pStr);
+DNode	*makeLinkedBTree(char *pStr);
 int	isOperator(int ch);
-int	isLegal(char* pStr);
+int	isLegal(char *pStr);
 
 // 이진 트리 순회: 깊이 우선 순회(전위.중위.후위 순회)
-void	Preorder(DNode* root);
-void	Inorder(DNode* root);
-void	Postorder(DNode* root);
+void	Preorder(DNode *root);
+void	Inorder(DNode *root);
+void	Postorder(DNode *root);
 
 // 이진 트리 순회: 너비 우선 순회
-void	Levelorder(DNode* root);
+void	Levelorder(DNode *root);
