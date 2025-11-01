6d7a9ae4f5ae148443eba851cbf0f4411efb3bd9
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-11-01 19:50:42 +0900

    Refactor LinkedBSTree to use consistent field names

--- c/08_LinkedBSTree.c
+++ c/08_LinkedBSTree.c
@@ -6,7 +6,7 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>		// malloc
+#include <stdlib.h>			// malloc
 #include <stdbool.h>		// bool, true, false
 #include "LinkedBSTree.h"	// DNode
 // #include "LinkedDNode.h"	// DNode
@@ -14,21 +14,21 @@
 // 깊이 우선 순회: 전위 순회(재귀적 용법)
 void	Preorder(DNode *root) {
 	if (root) {
-		printf("%3d", root->__data);
-		Preorder(root->__Llink);
-		Preorder(root->__Rlink);
+		printf("%3d", root->data);
+		Preorder(root->Llink);
+		Preorder(root->Rlink);
 	}
 }
 
 // 이진 검색 트리(BST): 데이터검색 -- 재귀적 용법
 DNode	*searchBST(DNode *root, element data) {
 	// 탐색 실패 또는 탐색 성공
-	if (root == NULL || data == root->__data)
+	if (root == NULL || data == root->data)
 		return root;
 
 	DNode	*tNode = NULL;
-	if (data < root->__data)	tNode = searchBST(root->__Llink, data);
-	else if (data > root->__data)	tNode = searchBST(root->__Rlink, data);
+	if (data < root->data)	tNode = searchBST(root->Llink, data);
+	else if (data > root->data)	tNode = searchBST(root->Rlink, data);
 	return tNode;
 }
 
@@ -41,12 +41,12 @@ DNode	*insertBST(DNode *root, element data) {
 	}
 	
 	// 중복된 데이터가 존재할 경우 데이터 삽입 불가능!!!
-	if(data == root->__data)
+	if(data == root->data)
 		printf("이미 같은 키가 있습니다!!! \n");
-	else if(data < root->__data)
-		root->__Llink = insertBST(root->__Llink, data);
-	else if(data > root->__data)
-		root->__Rlink = insertBST(root->__Rlink, data);
+	else if(data < root->data)
+		root->Llink = insertBST(root->Llink, data);
+	else if(data > root->data)
+		root->Rlink = insertBST(root->Rlink, data);
 	
 	return root;
 }
@@ -61,52 +61,52 @@ DNode	*deleteBST(DNode *root, element data) {
 
 	DNode	*pParent = NULL;	// 삭제할 노드의 부모 노드
 	DNode	*tNode = root;		// 삭제할 노드
-	DNode	*pChild;		// 삭제할 노드의 자식 노드
+	DNode	*pChild;			// 삭제할 노드의 자식 노드
 
 	// 삭제할 노드의 위치 탐색
-	while (data != tNode->__data) {
+	while (data != tNode->data) {
 		pParent = tNode;
-		if (data < tNode ->__data) tNode = tNode -> __Llink;
-		else tNode = tNode -> __Rlink;
+		if (data < tNode ->data) tNode = tNode ->Llink;
+		else tNode = tNode ->Rlink;
 	}
 
 	// 1) 삭제할 노드가 단말 노드인 경우
-	if ((tNode->__Llink == NULL) && (tNode->__Rlink == NULL)) {
+	if ((tNode->Llink == NULL) && (tNode->Rlink == NULL)) {
 		if (pParent == NULL)	root = NULL;
 		else if (pParent != NULL) {
-			if (pParent->__Llink == tNode)	pParent->__Llink = NULL;
-			else				pParent->__Rlink = NULL;
+			if (pParent->Llink == tNode)	pParent->Llink = NULL;
+			else							pParent->Rlink = NULL;
 		}
 	}
 
 	// 2) 삭제할 노드가 자식 노드를 한 개 가진 경우
-	else if ((tNode->__Llink == NULL) || (tNode->__Rlink == NULL)) {
+	else if ((tNode->Llink == NULL) || (tNode->Rlink == NULL)) {
 		// pChild : (후계자) 삭제할 노드의 자식 노드
-		if (tNode->__Llink != NULL)	pChild = tNode->__Llink;
-		else				pChild = tNode->__Rlink;
+		if (tNode->Llink != NULL)	pChild = tNode->Llink;
+		else						pChild = tNode->Rlink;
 
 		if (pParent == NULL)	root = pChild;
 		else {
-			if (pParent->__Llink == tNode) pParent->__Llink = pChild;
-			else                           pParent->__Rlink = pChild;
+			if (pParent->Llink == tNode) pParent->Llink = pChild;
+			else                         pParent->Rlink = pChild;
 		}
 	}
 	// 3) 삭제할 노드가 자식 노드를 두 개 가진 경우
 	else {	
 		// 왼쪽 서브 트리에서 후계자(최댓값) 찾기
 		DNode	*sParent = tNode;
-		DNode	*sNode = tNode->__Llink;
-		while (sNode->__Rlink != NULL) {
+		DNode	*sNode = tNode->Llink;
+		while (sNode->Rlink != NULL) {
 			sParent = sNode;
-			sNode = sNode->__Rlink;
+			sNode = sNode->Rlink;
 		}
 
-		if (sParent->__Llink == sNode)
-			sParent->__Llink = sNode->__Llink;
+		if (sParent->Llink == sNode)
+			sParent->Llink = sNode->Llink;
 		else
-			sParent->__Rlink = sNode->__Llink;
+			sParent->Rlink = sNode->Llink;
 
-		tNode->__data = sNode->__data;
+		tNode->data = sNode->data;
 		tNode = sNode;
 	}
 	free(tNode);
