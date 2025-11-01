f6319f8510e24920a03553af83ea67c04859022d
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-11-01 19:31:02 +0900

    Fix formatting in comments and includes

--- c/05_LinkedStack.c
+++ c/05_LinkedStack.c
@@ -2,15 +2,15 @@
 	스택: 알고리즘 구현(C) -- 단순연결리스트
 		파일명: LinkedStack.c
 			- 스택 생성.소멸	: stackCreate, stackDestroy
-			- 스택 상태		: stackEmpty
-			- 스택 크기		: stackSize
+			- 스택 상태			: stackEmpty
+			- 스택 크기			: stackSize
 			- 데이터 삽입.삭제	: push, pop
 			- 데이터 확인(peek)	: top
 			- 전체 데이터 출력	: printStack
 */
 
 #include <stdio.h>
-#include <stdlib.h>			// malloc, free
+#include <stdlib.h>				// malloc, free
 #include "LinkedStack.h"		// LinkedStack, SNode
 // #include "LinkedNode.h"		// SNode
 
