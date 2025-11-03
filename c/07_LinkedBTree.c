807f7f8842941cbc56ffe3f531d348e719490e10
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2025-11-03 20:34:55 +0900

    Refactor variable names for clarity in LinkedBTree

--- c/07_LinkedBTree.c
+++ c/07_LinkedBTree.c
@@ -1,50 +1,50 @@
 /*
-	이진 트리: 알고리즘 구현
+	이진 트리: 알고리즘 구현(C) -- 이중연결리스트
 		파일명: LinkedBTree.c
-			- 이진 트리 생성				: makeLinkedBTree
+			- 이진 트리 생성					: makeLinkedBTree
 			- 깊이 우선 순회(전위.중위.후위)	: Preorder, Inorder, Postorder
-			- 너비 우선 순회				: Levelorder
+			- 너비 우선 순회					: Levelorder
 */
 
 #include <stdio.h>
-#include <stdlib.h>			// malloc
-#include "LinkedStack.h"	// LinkedStack
-#include "LinkedQueue.h"	// LinkedQueue
-#include "LinkedBTree.h"	// DNode
-// #include "LinkedNode.h"	// SNode, DNode
-#include "Operators.h"		// isOperator,precedence, isLegal
+#include <stdlib.h>				// malloc
+#include "LinkedStack.h"		// LinkedStack
+#include "LinkedQueue.h"		// LinkedQueue
+#include "LinkedBTree.h"		// DNode
+// #include "LinkedNode.h"		// SNode, DNode
+#include "Operators.h"			// isOperator
 
 // 이진 트리 생성
 DNode* makeLinkedBTree(char *pStr) {
-	DNode		*temp;
+	DNode		*tNode;
 	LinkedStack	*Stack = stackCreate();
 	while (*pStr) {
 		// 공백 제거
 		while (*pStr == ' ')
 			pStr++;
 
-		temp = makeDNode(*pStr);
+		tNode = makeDNode(*pStr);
 
 		// 연산자일 경우: 스택에서 자식 노드를 구성할 주소를 pop
 		if (isOperator(*pStr)) {
-			temp->Rlink = (DNode*)top(Stack;		pop(Stack);
-			temp->Llink = (DNode*)top(Stack);		pop(Stack);
+			tNode->Rlink = (DNode*)top(Stack);		pop(Stack);
+			tNode->Llink = (DNode*)top(Stack);		pop(Stack);
 		}
-		push(Stack, (long long)temp);
+		push(Stack, (long long)tNode);
 		pStr++;
 	}
 	// 루트 노드
-	temp = (DNode*)top(Stack);
+	tNode = (DNode*)top(Stack);
 	pop(Stack);
 
 	stackDestroy(Stack);
-	return  temp;
+	return  tNode;
 }
 
 // 깊이 우선 순회: 전위 순회(재귀적 용법)
 void  Preorder(DNode *root) {
 	if (root) {
-		printf("%3c", root->__data);
+		printf("%3c", root->data);
 		Preorder(root->Llink);
 		Preorder(root->Rlink);
 	}
@@ -70,15 +70,15 @@ void  Postorder(DNode *root) {
 
 // 너비 우선 순회: 비재귀적 용법
 void  Levelorder(DNode *root) {
-	DNode		*temp;
+	DNode		*tNode;
 	LinkedQueue	*Queue = queueCreate();
 
 	enQueue(Queue, (long long)root);
-	while (!queueEempty(Queue))	{
-		temp = (DNode*)front(Queue);	deQueue(Queue);
-		printf("%3c", temp->__data);
-		if (temp->Llink)	enQueue(Queue, temp->Llink);
-		if (temp->Rlink)	enQueue(Queue, temp->Rlink);
+	while (!queueEempty(Queue)) {
+		tNode = (DNode*)front(Queue);	deQueue(Queue);
+		printf("%3c", tNode->data);
+		if (tNode->Llink)	enQueue(Queue, tNode->Llink);
+		if (tNode->Rlink)	enQueue(Queue, tNode->Rlink);
 	}
 	queueDestroy(Queue);
 }
